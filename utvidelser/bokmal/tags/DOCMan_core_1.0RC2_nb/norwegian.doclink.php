08d71b10ba843a91279fea7ec159a24522d0f681
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-03-14 12:02:16 +0000

    Overføring fra gammel tags-lager

---
+++ utvidelser/bokmal/tags/DOCMan_core_1.0RC2_nb/norwegian.doclink.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* DOCMan 1.3.0 for Mambo 4.5.1 CMS
+* @version $Id: norwegian.doclink.php,v 1.0 2005/12/06 22:46:22 rued Exp $
+* @package DOCMan_1.3.0
+* @copyright (C) 2003 - 2005 The DOCMan Development Team
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* Oficial website: http://www.mambodocman.com/
+* -------------------------------------------
+* Norwegian language file
+* Translator: Rune Rasmussen - http://www.syntaxerror.no/
+* Email: nospam@syntaxerror.no
+* Revision: 1.1
+* Date: 2006.03.19
+*/
+// ensure this file is being included by a parent file */
+defined("_VALID_MOS") or die("Direct Access to this location is not allowed.");
+
+DEFINE("_DML_DCL_TITLE"			, "Sett inn lenke");
+DEFINE("_DML_DCL_LOADING"		, "Laster ...");
+DEFINE("_DML_DCL_MANAGER"		, "Behandler");
+DEFINE("_DML_DCL_SETTINGS"		, "Innstillinger");
+
+DEFINE("_DML_DCL_CATEGORY"		, "Kategori");
+DEFINE("_DML_DCL_UP"			, "Opp");
+
+DEFINE("_DML_DCL_URL"			, "Nettadresse");
+DEFINE("_DML_DCL_CAPTION"		, "Overskrift");
+DEFINE("_DML_DCL_INSERTICON"	, "Sett inn filikon");
+DEFINE("_DML_DCL_INSERTSIZE"	, "Sett inn filstørrelse");
+DEFINE("_DML_DCL_INSERTDATE"	, "Sett inn fildato");
+?>
