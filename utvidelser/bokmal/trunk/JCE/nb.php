18735b6f2a6c85f319cfd7625087aa5089d68417
jenscski <jenscski@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-03-15 09:43:28 +0000

    Lagt til norsk oversettelse av JCE

---
+++ utvidelser/bokmal/trunk/JCE/nb.php
@@ -0,0 +1,61 @@
+<?php
+// Ferdig oversatt, 31. mars 2006
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+//Common
+DEFINE('_JCE_VERSION', 'Versjon');
+DEFINE('_JCE_DATE', 'Dato');
+DEFINE('_JCE_AUTHOR', 'Forfatter');
+DEFINE('_JCE_AUTHOR_EMAIL', 'Forfatters e-post');
+DEFINE('_JCE_AUTHOR_URL', 'Forfatters nettlenke');
+DEFINE('_JCE_PUBLISHED', 'Publisert');
+
+//Layout Editor
+DEFINE('_JCE_LAYOUT_MSG','Bare knapper tilhørende kommandoer og programtillegg som er publisert vil vises her. Dra og slipp knapper fra en rad til en annen, eller innen samme rad for å justere layouten. Det grå området representerer tekstbehandlere. Knappene må passe inn i dette området. De blå rektanglene indikerer grensene for radene.');
+DEFINE('_JCE_LAYOUT_HEADING', 'JCE-layoutbehandler');
+//
+
+//Language Manager
+DEFINE('_JCE_LANG_HEADING', 'JCE-språkbehandler');
+DEFINE('_JCE_LANG_LANG', 'Språk');
+
+//Language Installer
+DEFINE('_JCE_LANG_HEADING_INSTALL', 'Installer nytt språk til JCE');
+DEFINE('_JCE_LANG_BACK', 'Tilbake til språkbehandler');
+//
+
+//Global Installer
+DEFINE('_JCE_INS_PACKAGE_UPLOAD', 'Last opp pakke');
+DEFINE('_JCE_INS_PACKAGE_FILE', 'Pakke');
+DEFINE('_JCE_INS_INSTALL', 'Installer fra mappe');
+DEFINE('_JCE_INS_INSTALL_DIR', 'Installasjonsmappe');
+DEFINE('_JCE_INS_UPLOAD_BUTTON', 'Last opp og installer');
+DEFINE('_JCE_INS_INSTALL_BUTTON', 'Installer');
+//
+
+//Plugins
+DEFINE('_JCE_PLUGIN_PLUGIN', 'Programtillegg');
+DEFINE('_JCE_PLUGIN_PLUGCOM', 'Programtillegg/kommando');
+DEFINE('_JCE_PLUGIN_NAME', 'Navn');
+DEFINE('_JCE_PLUGIN_HEADING', 'JCE-programtilleggbehandler');
+DEFINE('_JCE_PLUGIN_FILTER', 'Filter');
+DEFINE('_JCE_PLUGIN_ACCESS_LIST', 'Adgangsliste');
+DEFINE('_JCE_PLUGIN_ACCESS_LVL', 'Adgangsnivå');
+DEFINE('_JCE_PLUGIN_CORE', 'Kjerne');
+DEFINE('_JCE_PLUGIN_ORDER', 'Sortering');
+DEFINE('_JCE_PLUGIN_ROW', 'Rad');
+DEFINE('_JCE_PLUGIN_TYPE', 'Type');
+DEFINE('_JCE_PLUGIN_ICON', 'Ikon');
+DEFINE('_JCE_PLUGIN_LAYOUT_ICON', 'Layoutikon');
+DEFINE('_JCE_PLUGIN_DESC', 'Beskrivelse');
+DEFINE('_JCE_PLUGIN_EDIT', 'Rediger');
+DEFINE('_JCE_PLUGIN_NEW', 'Ny');
+DEFINE('_JCE_PLUGIN_DETAILS', 'Programtilleggdetaljer');
+DEFINE('_JCE_PLUGIN_PARAMS', 'Parametere');
+DEFINE('_JCE_PLUGIN_ELMS', 'Elementer');
+//Plugin Installer
+DEFINE('_JCE_PLUGINS_INSTALL_HEADING','Installert nye JCE-programtillegg');
+DEFINE('_JCE_PLUGINS_INSTALL_MSG', 'Bare programtillegg som kan avinstalleres vises - noen kjerneprogramtillegg kan ikke fjernes.');
+DEFINE('_JCE_PLUGIN_NONE', 'Det er ingen ikke-kjerneprogramtillegg installert');
+?>
---
+++ utvidelser/bokmal/trunk/JCE/plugins/filemanager/langs/nb.js
@@ -0,0 +1,6 @@
+// NB lang variables
+// Ferdig oversatt, 31. mars 2006
+
+tinyMCE.addToLang('filemanager',{
+desc : 'Sett inn lenke til fil'
+});
