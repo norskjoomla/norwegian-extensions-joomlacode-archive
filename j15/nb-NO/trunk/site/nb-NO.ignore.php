d3942931a575f404128f7107f5f6af9c1d3528da
Jens-Christian Skibakk <jenscski@users.noreply.github.com>
2007-04-19 05:16:07 +0000

    svn:keywords: Id

--- j15/nb-NO/trunk/site/nb-NO.ignore.php
+++ j15/nb-NO/trunk/site/nb-NO.ignore.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Id: nb-NO.ignore.php 169 2007-02-07 07:27:44Z jens $
+ * $Id$
  * author Joomla! i Norge
  * copyright (C) 2005 - 2007 Joomla! i Norge. All rights reserved.
  * license http://www.gnu.org/copyleft/gpl.html GNU/GPL
