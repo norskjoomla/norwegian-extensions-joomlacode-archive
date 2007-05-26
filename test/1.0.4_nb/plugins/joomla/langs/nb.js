699ac208387dd89afb745454ed0fb48b1e46dc8a
lbmyren <lbmyren@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-05-26 07:54:57 +0000

    Slettet fil/mappe

--- test/1.0.4_nb/plugins/joomla/langs/nb.js
+++
@@ -1,8 +0,0 @@
-// NB lang variables
-
-tinyMCE.addToLang('joomla',{
-mospagebreak_desc : 'Sett inn sideskift',
-mospagebreak_title : 'Tittel',
-mospagebreak_heading : 'Overskrift',
-mosimage_desc : 'Sett inn mosimage',
-});
