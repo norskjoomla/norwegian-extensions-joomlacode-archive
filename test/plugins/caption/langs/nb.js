e7becec96e960aad32da804ed43b809bbd300b75
lbmyren <lbmyren@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-05-26 07:52:42 +0000

    Slettet fil/mappe

--- test/plugins/caption/langs/nb.js
+++
@@ -1,8 +0,0 @@
-// DK lang variables - Transl.:Ronny Buelund - Corr.:
-
-tinyMCE.addToLang('caption',{
-desc : 'Sett inn/Rediger bildetekst',
-delete_desc : 'Slett bildetekst',
-web_safe : 'Nettsikker',
-def : 'Standard'
-});
--- test/plugins/visualchars/langs/nb.js
+++
@@ -1,7 +0,0 @@
-// NB lang variables
-
-// Ferdig oversatt, 25. mai 2007
-
-tinyMCE.addToLang('visualchars',{
-desc : 'Skjulte formateringssymboler av/på.'
-});
