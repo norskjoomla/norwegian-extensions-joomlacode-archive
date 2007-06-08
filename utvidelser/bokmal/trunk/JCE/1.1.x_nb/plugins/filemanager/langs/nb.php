ffb23ec079a37f0ef0e2dc74b40aac8661343c7f
lbmyren <lbmyren@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-06-08 21:03:53 +0000

    Oppdatert jce_filemanager til v.111

--- utvidelser/bokmal/trunk/JCE/1.1.x_nb/plugins/filemanager/langs/nb.php
+++ utvidelser/bokmal/trunk/JCE/1.1.x_nb/plugins/filemanager/langs/nb.php
@@ -1,5 +1,5 @@
 <?php
-// en lang variables
+// no-nb lang variables
 //Names
 $fmlang['desc'] = 'Filbehandler';
 $fmlang['ins_file'] = 'Sett inn fil';
@@ -7,4 +7,13 @@ $fmlang['file_list'] = 'Filliste';
 $fmlang['icon'] = 'Inkluder ikon';
 $fmlang['date'] = 'Inkluder dato';
 $fmlang['size'] = 'Inkluder størrelse';
+
+/*Help Keys
+ *The array key is used as the identifier sent with the help url to access the relevant help file using the findkey task option.
+ *The value is used as the help item label.
+*/
+$ph['about'] = 'Hva er File Manager?';
+$ph['interface'] = 'File Manager grensesnitt';
+$ph['insert'] = 'Setter inn en fil';
+
 ?>
