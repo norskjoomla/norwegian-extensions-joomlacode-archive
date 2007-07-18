4c408341e0b681707a46d1ed5bb292c8d2528f8c
Jens-Christian Skibakk <jenscski@users.noreply.github.com>
2007-07-18 11:31:22 +0000

    http://www.gnu.org/licenses/gpl-2.0.html

--- j15/nb-NO/trunk/site/nb-NO.ignore.php
+++ j15/nb-NO/trunk/site/nb-NO.ignore.php
@@ -3,7 +3,7 @@
  * $Id$
  * author Joomla! i Norge
  * copyright (C) 2005 - 2007 Joomla! i Norge. All rights reserved.
- * license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
 $search_ignore[] = "og";
