e17f12c3004cc99b21c1f6d8d5e38495f60d8c9f
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-07-22 09:30:55 +0000

    Opprettet kopi av revisjon 137 for bruk mot språkvasksiden, tenker at vi først laster inn endringer her for å sjekke - før de legges i/slås sammen med trunk

---
+++ j15/nb-NO/branches/site/nb-NO.ignore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * $Id$
+ * author Joomla! i Norge
+ * copyright (C) 2005 - 2007 Joomla! i Norge. All rights reserved.
+ * license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+$search_ignore[] = "og";
+$search_ignore[] = "i";
+$search_ignore[] = "den";
+$search_ignore[] = "på";
+?>
