fc05137536688285c729f68b29da4d7b7d1d1292
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2007-09-05 19:08:24 +0000

    Endret til norsk datoformat og lagt inn manglende mellomrom

--- j15/nb-NO/trunk/lokalisering/includes/js/calendar/lang/calendar-nb-NO.js
+++ j15/nb-NO/trunk/lokalisering/includes/js/calendar/lang/calendar-nb-NO.js
@@ -24,12 +24,12 @@ Calendar._MN = new Array
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["TOGGLE"] = "Bytt første date i uken";
+Calendar._TT["TOGGLE"] = "Bytt første dag i uken";
 Calendar._TT["PREV_YEAR"] = "Forrige år (hold nede for meny)";
 Calendar._TT["PREV_MONTH"] = "Forrige måned (hold nede for meny)";
 Calendar._TT["GO_TODAY"] = "Gå til i dag";
-Calendar._TT["NEXT_MONTH"] = "Neste måned(hold nede for meny)";
-Calendar._TT["NEXT_YEAR"] = "Neste år(hold nede for meny)";
+Calendar._TT["NEXT_MONTH"] = "Neste måned (hold nede for meny)";
+Calendar._TT["NEXT_YEAR"] = "Neste år (hold nede for meny)";
 Calendar._TT["SEL_DATE"] = "Velg dato";
 Calendar._TT["DRAG_TO_MOVE"] = "Klikk og dra for å flytte";
 Calendar._TT["PART_TODAY"] = " (i dag)";
@@ -39,7 +39,7 @@ Calendar._TT["CLOSE"] = "Lukk";
 Calendar._TT["TODAY"] = "I dag";
 
 // date formats
-Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
-Calendar._TT["TT_DATE_FORMAT"] = "D, M d";
+Calendar._TT["DEF_DATE_FORMAT"] = "dd-mm-y";
+Calendar._TT["TT_DATE_FORMAT"] = "D. d M.";
 
 Calendar._TT["WK"] = "Uke";
