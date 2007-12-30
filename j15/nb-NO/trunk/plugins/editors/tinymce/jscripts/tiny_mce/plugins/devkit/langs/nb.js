48a7e8156d6f7e87904bf6063630f324b657f6b8
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2007-12-30 22:36:39 +0000

    Endret Copyrigt år fra 2006/07 til 2008

--- j15/nb-NO/trunk/plugins/editors/tinymce/jscripts/tiny_mce/plugins/devkit/langs/nb.js
+++ j15/nb-NO/trunk/plugins/editors/tinymce/jscripts/tiny_mce/plugins/devkit/langs/nb.js
@@ -1,6 +1,6 @@
 // version 1.5.x $Id: nb.js 129 2006-11-07 18:03:20Z jens $
 // author Joomla! i Norge http://www.joomlainorge.no oversettelser@joomlainorge.no
-// copyright Copyright (C) 2005 - 2006 Joomla! i Norge. All rights reserved.
+// copyright Copyright (C) 2005 - 2008 Joomla! i Norge. All rights reserved.
 // license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 
 tinyMCE.addToLang('devkit',{
