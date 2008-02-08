4ef3996a9f19c35a4291235c52adcea75ec4a835
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2008-02-08 13:27:15 +0000

    Tagger komplett 1.5 Stabil

---
+++ j15/nb-NO/tags/nb-NO_Joomla_1.5_Stable_Khepri_KOMP/plugins/editors/tinymce/jscripts/tiny_mce/langs/nb.js
@@ -0,0 +1,44 @@
+// version 1.5.x $Id: nb.js 117 2006-10-24 12:49:07Z jens $
+// author Joomla! i Norge http://www.joomlainorge.no oversettelser@joomlainorge.no
+// copyright Copyright (C) 2005 - 2008 Joomla! i Norge. All rights reserved.
+// license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+
+tinyMCE.addToLang('',{
+bold_desc : 'Fet',
+italic_desc : 'Kursiv',
+underline_desc : 'Understreking',
+striketrough_desc : 'Gjennomstreking',
+justifyleft_desc : 'Venstrestill',
+justifycenter_desc : 'Sentrer',
+justifyright_desc : 'Høyrestill',
+justifyfull_desc : 'Blokkjustering',
+bullist_desc : 'Punktliste',
+numlist_desc : 'Nummerliste',
+outdent_desc : 'Reduser innrykk',
+indent_desc : 'Øk innrykk',
+undo_desc : 'Angre',
+redo_desc : 'Gjør om',
+link_desc : 'Opprett/endre lenke',
+unlink_desc : 'Fjern lenke',
+image_desc : 'Sett inn/endre bilde',
+cleanup_desc : 'Rens grisete kode',
+focus_alert : 'Editoren må være i fokus før du benytter denne kommandoen.',
+edit_confirm : 'Vil du benytte WYSIWYG editoren for dette tekstfelt?',
+insert_link_title : 'Opprett/endre lenke',
+insert : 'Sett inn',
+update : 'Oppdater',
+cancel : 'Avbryt',
+insert_link_url : 'Lenkens nettadresse',
+insert_link_target : 'Vindu',
+insert_link_target_same : 'Åpne i samme vindu',
+insert_link_target_blank : 'Åpne i nytt vindu',
+insert_image_title : 'Opprett/endre bilde',
+insert_image_src : 'Bildets nettadresse',
+insert_image_alt : 'Bildets beskrivelse',
+help_desc : 'Hjelp',
+bold_img : 'bold_de_se.gif',
+italic_img : 'italic_de_se.gif',
+underline_img : 'underline.gif',
+clipboard_msg : 'Kopier/Klipp ut/Lim inn er ikke tilgjengelig i Mozilla/Firefox. Vil du ha mere informasjon om dette?',
+popup_blocked : 'En popup-blokker har sperret et vindu med viktig funksjonalitet, for å benytte deg av denne funksjonaliteten må du deaktivere popup-blokkeren for dette nettstedet.'
+});
