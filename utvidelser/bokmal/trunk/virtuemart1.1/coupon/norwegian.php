b41d60a7a3170b601f63368b37dc6afd414eaee1
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2008-04-12 10:56:41 +0000

    Klar til test/bruk

--- utvidelser/bokmal/trunk/virtuemart1.1/coupon/norwegian.php
+++ utvidelser/bokmal/trunk/virtuemart1.1/coupon/norwegian.php
@@ -19,13 +19,13 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
 global $VM_LANG;
 $langvars = array (
 	'CHARSET' => 'ISO-8859-1',
-	'PHPSHOP_COUPON_EDIT_HEADER' => 'Oppdater rabatt',
+	'PHPSHOP_COUPON_EDIT_HEADER' => 'Oppdater rabatt/gavekort',
 	'PHPSHOP_COUPON_CODE_HEADER' => 'Kode',
 	'PHPSHOP_COUPON_PERCENT_TOTAL' => 'Prosent eller totalt',
-	'PHPSHOP_COUPON_TYPE' => 'Rabatt Type',
+	'PHPSHOP_COUPON_TYPE' => 'Rabattype',
 	'PHPSHOP_COUPON_TYPE_TOOLTIP' => 'En gaverabatt slettes når den er brukt på en ordre. En permanent rabatt kan brukes flere ganger.',
-	'PHPSHOP_COUPON_TYPE_GIFT' => 'Gave Rabatt',
-	'PHPSHOP_COUPON_TYPE_PERMANENT' => 'Permanent Rabatt',
+	'PHPSHOP_COUPON_TYPE_GIFT' => 'Gaverabatt',
+	'PHPSHOP_COUPON_TYPE_PERMANENT' => 'Permanent rabatt',
 	'PHPSHOP_COUPON_VALUE_HEADER' => 'Verdi',
 	'PHPSHOP_COUPON_PERCENT' => 'Prosent',
 	'PHPSHOP_COUPON_TOTAL' => 'Totalt'
