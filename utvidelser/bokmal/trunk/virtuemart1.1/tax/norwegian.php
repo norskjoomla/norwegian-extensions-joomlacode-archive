7334c608d4423c92fb0f0b2f190b48a4c5b0b57e
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2008-04-12 09:29:38 +0000

    Klar til test/bruk

--- utvidelser/bokmal/trunk/virtuemart1.1/tax/norwegian.php
+++ utvidelser/bokmal/trunk/virtuemart1.1/tax/norwegian.php
@@ -19,12 +19,12 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
 global $VM_LANG;
 $langvars = array (
 	'CHARSET' => 'ISO-8859-1',
-	'PHPSHOP_TAX_LIST_LBL' => 'Liste over mva verdier',
-	'PHPSHOP_TAX_LIST_STATE' => 'Mva stat eller region',
-	'PHPSHOP_TAX_LIST_COUNTRY' => 'Mva land',
-	'PHPSHOP_TAX_FORM_LBL' => 'Legg til info for satsen',
-	'PHPSHOP_TAX_FORM_STATE' => 'Mva stat eller region',
-	'PHPSHOP_TAX_FORM_COUNTRY' => 'Mva land',
-	'PHPSHOP_TAX_FORM_RATE' => 'Mva sats'
+	'PHPSHOP_TAX_LIST_LBL' => 'Liste over mva-verdier',
+	'PHPSHOP_TAX_LIST_STATE' => 'Mva-region',
+	'PHPSHOP_TAX_LIST_COUNTRY' => 'Mva-land',
+	'PHPSHOP_TAX_FORM_LBL' => 'Legg til infomasjon om satsen',
+	'PHPSHOP_TAX_FORM_STATE' => 'Mva-region',
+	'PHPSHOP_TAX_FORM_COUNTRY' => 'Mva-land',
+	'PHPSHOP_TAX_FORM_RATE' => 'Mva-sats'
 ); $VM_LANG->initModule( 'tax', $langvars );
 ?>
