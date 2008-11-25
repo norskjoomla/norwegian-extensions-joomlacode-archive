ed8d06e6c119f36fa35a4bb46ab71a3f86d10dac
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2008-11-25 16:17:08 +0000

    Nynorsk lastes inn, site-filer skal vistnok være klare for bruk.

---
+++ j15/nn-NO/trunk/language/nn-NO/nn-NO.ignore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * $Id: nb-NO.ignore.php 437 2008-07-16 05:38:56Z jenscski $
+ * author Joomla! i Norge
+ * copyright (C) 2005 - 2008 Joomla! i Norge. All rights reserved.
+ * license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+$search_ignore[] = "og";
+$search_ignore[] = "i";
+$search_ignore[] = "den";
+$search_ignore[] = "på";
+?>
