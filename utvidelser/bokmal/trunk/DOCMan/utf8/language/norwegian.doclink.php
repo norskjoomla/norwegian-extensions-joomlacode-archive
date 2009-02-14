182127435c1b925e331236ecba4c699110b27aee
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2009-02-14 22:40:49 +0000

    Berget ut og laster opp DOCMan filene lokalt, siden JoomlaTools bare kødder de til gang på gang

---
+++ utvidelser/bokmal/trunk/DOCMan/utf8/language/norwegian.doclink.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DOCman 1.4.x - Joomla! Document Manager
+ * @version $Id$
+ * @package DOCman_1.4
+ * @copyright (C) 2003-2007 The DOCman Development Team
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.joomlatools.org/ Official web site
+ **/
+defined('_VALID_MOS') or die('Restricted access');
+
+/**
+ * TRANSLATORS:
+ * PLEASE CHANGE THE INFO BELOW
+ * 
+ * Default Norwegian language file
+ *
+ * Creator:  Rune Rasmussen
+ * Website:  http://www.syntaxerror.no/
+ * E-mail:   nospam@syntaxerror.no
+ * Revision: 1.4
+ * Date:     September 2007
+ **/
+
+define('_DML_DCL_TITLE', "Sett inn lenke");
+define('_DML_DCL_LOADING', "Laster ...");
+define('_DML_DCL_MANAGER', "Behandler");
+define('_DML_DCL_SETTINGS', "Innstillinger");
+
+define('_DML_DCL_CATEGORY', "Kategori");
+define('_DML_DCL_UP', "Opp");
+
+define('_DML_DCL_URL', "Nettadresse");
+define('_DML_DCL_CAPTION', "Beskrivelse");
+define('_DML_DCL_INSERTICON', "Sett inn filikon");
+define('_DML_DCL_INSERTSIZE', "Sett inn filstørrelse");
+define('_DML_DCL_INSERTDATE', "Sett inn endringsdato for fil");
