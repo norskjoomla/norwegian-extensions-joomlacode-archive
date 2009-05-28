733c0b0d37c73d46e14a057ab145569b35cc4018
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-05-28 18:51:45 +0000

    Fra Kristian, fullført.

--- utvidelser/nBill/norwegian/carriage.norwegian.php
+++ utvidelser/nBill/norwegian/carriage.norwegian.php
@@ -3,6 +3,7 @@
 * @version 1.1.x
 * @package nBill
 * @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
 * @license Commercial
 * 
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
@@ -21,31 +22,31 @@
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Carriage
-define("INV_CARRIAGE_TITLE", "Carriage Rates");
-define("INV_CARRIAGE_INTRO", "This is optional. If you add carriage costs onto the price of your products, you can specify the rates here. You can also specify whether carriage is taxable, and if so, apply a different rate of tax for the carriage portion of the invoice.");
-define("INV_CARRIAGE_COUNTRY", "Country");
-define("INV_CARRIAGE_PRICE_PER_UNIT", "Unit Price");
-define("INV_EDIT_CARRIAGE_RATE", "Edit Carriage Rate");
-define("INV_NEW_CARRIAGE_RATE", "New Carriage Rate");
-define("INV_SERVICE_NAME_REQUIRED", "Please enter the name of this service");
-define("INV_COUNTRY_REQUIRED", "Please select the country to which this service applies");
-define("INV_CARRIAGE_DETAILS", "Carriage Details)");
-define("INV_CARRIAGE_CODE", "Code");
-define("INV_NET_PRICE_PER_UNIT", "Net Price per Unit");
-define("INV_CARRIAGE_FIXED", "Fixed Price Carriage?");
-define("INV_CARRIAGE_IS_TAXABLE", "Is Carriage Taxable?");
-define("INV_CARRIAGE_TAX_RATE", "Tax Rate if Different");
-define("INV_INSTR_CARRIAGE_SERVICE", "Enter a descriptive name for the service (eg. 'First Class', 'Recorded Delivery')");
-define("INV_INSTR_CARRIAGE_CODE", "Enter your own short code for this service (optional, but if present is used to sort the delivery options alphabetically).");
-define("INV_INSTR_CARRIAGE_COUNTRY", "Select the country to which this service applies.");
-define("INV_INSTR_NET_PRICE_PER_UNIT", "If you charge a fixed price per invoice for carriage, enter that amount here and select 'yes' to the fixed price option below. If your carriage costs depend on the product size/weight/quantity, enter the amount in figures that represents the smallest common denominator for your products' carriage fees. You can then specify a multiplier of this value on a per-product basis which will be used to calculate the carriage fees that appear on invoices. Do not include a currency symbol.");
-define("INV_INSTR_CARRIAGE_FIXED", "If you charge a fixed price per invoice for carriage, select 'yes', and the value you entered in 'Net Price per Unit' will be applied as a fixed fee to the whole invoice (instead of as a unit price). If your carriage costs depend on the product size/weight/quantity, select 'no'.");
-define("INV_INSTR_CARRIAGE_IS_TAXABLE", "Indicate whether value-added tax is to be charged for carriage amounts.");
-define("INV_INSTR_CARRIAGE_TAX_RATE", "If carriage is taxable AND the tax for carriage is charged at a different rate to normal, enter the carriage tax rate here (as a percentage, but without the % sign)");
-define("INV_CARRIAGE_PRICE_INTRO", "Enter the price of the carriage for each currency that you want to sell in.");
+define("INV_CARRIAGE_TITLE", "Fraktsatser");
+define("INV_CARRIAGE_INTRO", "Dette er valgfritt. Hvis du legger på fraktkostnader på prisen på produktene dine, kan du angi satsene her. Du kan også angi om transporten er MVA-pliktig, og i så fall bruke en annen sats av MVA for transportdelen av fakturaen.");
+define("INV_CARRIAGE_COUNTRY", "Land");
+define("INV_CARRIAGE_PRICE_PER_UNIT", "Enhetspris");
+define("INV_EDIT_CARRIAGE_RATE", "Rediger fraktsatser");
+define("INV_NEW_CARRIAGE_RATE", "Ny fraktsats");
+define("INV_SERVICE_NAME_REQUIRED", "Fyll inn navnet på denne tjenesten");
+define("INV_COUNTRY_REQUIRED", "Velg land hvor denne tjenesten gjelder");
+define("INV_CARRIAGE_DETAILS", "Fraktdetaljer");
+define("INV_CARRIAGE_CODE", "Kode");
+define("INV_NET_PRICE_PER_UNIT", "Nettopris pr. enhet");
+define("INV_CARRIAGE_FIXED", "Fastpris?");
+define("INV_CARRIAGE_IS_TAXABLE", "Er frakten MVA plikig?");
+define("INV_CARRIAGE_TAX_RATE", "MVA-sats om forskjellig");
+define("INV_INSTR_CARRIAGE_SERVICE", "Skriv inn et beskrivende navn for tjenesten (Foreksempel 'Ekspress over natten', 'Levering neste dag')");
+define("INV_INSTR_CARRIAGE_CODE", "Angi din egen kode for denne tjenesten (valgfritt, men brukes til å sortere leveringsmuligheter alfabetisk).");
+define("INV_INSTR_CARRIAGE_COUNTRY", "Velg land der denne tjenesten gjelder.");
+define("INV_INSTR_NET_PRICE_PER_UNIT", "Hvis du har betalt fastpris for transport, angir du beløpet her og velge "ja" på Fastpris alternativet nedenfor. Hvis fraktprisen avhenger av produktets størrelse / tyngde eller mengde, skriver du beløpet som representerer den minste fellesnevneren for produktenes fraktavgift. Deretter kan du angi en multiplikator på denne verdien som vil bli brukt til å beregne transportavgifter som vises på fakturaene. Ikke inkluder valutasymbol.");
+define("INV_INSTR_CARRIAGE_FIXED", "Hvis du har betalt fastpris per faktura for transport, velger du "ja", og verdien du skrev inn i "Nettopris pr enhet" vil bli brukt som et fast gebyr for hele fakturaen (i stedet for som en enhet). Hvis fraktprisen avhenger av produktets størrelse / tyngde eller mengde, velger du "nei".");
+define("INV_INSTR_CARRIAGE_IS_TAXABLE", "Indiker om det skal betales moms av fraktprisen");
+define("INV_INSTR_CARRIAGE_TAX_RATE", "Hvis transporten er MVA pålagdt OG avgiften for transport er annerledels enn til vanlig, skriver du inn transportens MVA sats her (i prosent, men uten prosenttegnet %)");
+define("INV_CARRIAGE_PRICE_INTRO", "Angi prisen for transport for hver valuta som du vil selge i.");
 define("INV_CARRIAGE_ID", "ID");
 
 //Version 1.2.0
-define("INV_CARRIAGE_TRACKING_URL", "Carriage Tracking URL");
-define("INV_INSTR_CARRIAGE_TRACKING_URL", "If the courier allows online tracking of parcels, enter the URL here, and use ## (double hash) as a placeholder for the tracking ID. When you ship the package, you can enter the actual tracking ID on the order record. This will then be combined with the URL recorded here to generate a link in the website front-end that will allow the user to track their goods (subject to this being allowed within the display options).");
+define("INV_CARRIAGE_TRACKING_URL", "Transportens nettadresse for sporing");
+define("INV_INSTR_CARRIAGE_TRACKING_URL", "Hvis transportøren tillater elektronisk sporing av pakker, angi nettadressen her, og bruk ## (dobbel hash) som plassholder for sporingsnummer. Når du sender pakken kan du angi det riktige sporingsnummeret på ordren. Denne vil da bli slått sammen med nettadressen registrert her, for å generere en lenke på nettsidens brukerside som gjør det mulig for brukeren å spore sin forsendelse (om dette lar seg gjøre i henhold til visningsalternativene).");
 ?>
