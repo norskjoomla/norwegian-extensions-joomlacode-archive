f2c2808d0df35c245e117d887916d37a55529dd9
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-05-28 21:33:44 +0000

    Fra Petter, fullført

--- utvidelser/nBill/norwegian/credits.norwegian.php
+++ utvidelser/nBill/norwegian/credits.norwegian.php
@@ -3,6 +3,7 @@
 * @version 1.1.x
 * @package nBill
 * @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
 * @license Commercial
 * 
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
@@ -20,36 +21,36 @@
 /** ensure this file is being included by a parent file */
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-define("INV_CREDITS_TITLE", "Credit Notes (Refunds)");
-define("INV_CREDITS_INTRO", "Whenever you issue a refund, you should also produce an accompanying credit note detailing what the refund was for, and including a tax breakdown, if applicable.");
-define("INV_INVOICE_NUMBER_CR", "Credit Note Number");
-define("INV_BILLING_NAME_CR", "Payee");
-define("INV_INVOICE_DATE_CR", "Credit Date");
-define("INV_FIRST_ITEM_CR", "First Item on Credit Note");
-define("INV_EDIT_INVOICE_CR", "Edit Credit Note");
-define("INV_EMAIL_NOW_CR", "Send this credit note to the client by e-mail now");
-define("INV_BILLING_NAME_REQUIRED_CR", "Please enter the payee name");
+define("INV_CREDITS_TITLE", "Kreddittnoter (Refusjoner)");
+define("INV_CREDITS_INTRO", "N&aring;r du har sendt en tilbakebetaling, b&oslash;r du ogs&aring; lage et medf&oslash;lgende kredittnotat med informasjon om hva tilbakebetalingen gjelder, inkludert skatt, hvis aktuelt.");
+define("INV_INVOICE_NUMBER_CR", "Kredittnote nummer");
+define("INV_BILLING_NAME_CR", "Utbetalingsmottaker");
+define("INV_INVOICE_DATE_CR", "Kreditt dato");
+define("INV_FIRST_ITEM_CR", "F&oslash;rste element p&aring; kredittnote");
+define("INV_EDIT_INVOICE_CR", "Rediger kredittnote");
+define("INV_EMAIL_NOW_CR", "Send denne kredittnoten til en klient via epost n&aring;");
+define("INV_BILLING_NAME_REQUIRED_CR", "Vennligst skriv utbetalingsmottakers navn");
 define("INV_BILLING_ADDRESS_REQUIRED_CR", "Please enter the payee address");
-define("INV_NEW_INVOICE_CR", "New Credit Note");
-define("INV_INVOICE_DETAILS_CR", "Credit Note Details");
-define("INV_INSTR_INVOICE_NUMBER_CR", "<strong>Note:</strong> Leave blank if adding a new credit note - the component will automatically assign the next available credit note number.");
-define("INV_INSTR_BILLING_NAME_CR", "The name of the person and/or company being refunded.");
-define("INV_BILLING_ADDRESS_CR", "Payee Address");
-define("INV_INVOICE_ITEMS_CR", "Credit Note Items");
-define("INV_INSTR_INVOICE_PAID_IN_FULL_CR", "<strong>WARNING!</strong> Setting this value from here overrides the normal process of creating an 'Expenditure' item. This is OK if you do not intend to use the expenditure feature, but it is recommended that you mark credit notes as paid by clicking on the red 'X' on the credit note list, rather than setting the value here.");
-define("INV_INSTR_VENDOR_NAME_CR", "Enter the name that you want to appear on your credit notes");
-define("INV_INSTR_VENDOR_ADDRESS_CR", "Enter the address that you want to appear on your credit notes");
-define("INV_EMAIL_INVOICE_SUBJECT_CR", "Credit Note %s from %s");
-define("INV_EMAIL_INVOICE_INTRO_CR", "Dear %s,\n\nThe following credit note has been generated for you:");
-define("INV_EMAIL_INVOICE_FOOTER_CR", "You can see all of your invoices/credit notes online by logging in at %s. If you no longer wish to receive notification by e-mail, simply log in and click on `My Profile` in the `My Account` area to set your preferences.\n\nRegards,\n%s");
-define("INV_EMAIL_INVOICE_ALT_TEXT_CR", "You are seeing this message because your e-mail client does not support HTML messages (or if you are viewing it from within a 'Junk e-mail' folder, the HTML may be suppressed). In order to see your credit note, please either view this e-mail in HTML mode, or login at %s to view your credit note online.");
-define("INV_EMAIL_INVOICE_NOTIFICATION_CR", "Dear %s,\n\nThis e-mail is to inform you that a new credit note has been generated for you.\n\n");
-define("INV_EMAIL_INVOICE_FOOTER_USERNAME_CR", "You can see all of your invoices/credit notes online by logging in at %s (your username is %s). If you no longer wish to receive notification by e-mail, simply log in and click on `My Profile` in the `My Account` area to set your preferences.\n\nRegards,\n%s");
-define("INV_SEND_EMAIL_INTRO_CR", "Please select the type of e-mail to send (HTML or plain text). You may also override the e-mail address, contact name, and message if you wish by specifying your own values here. If you leave these blank, " . INV_BRANDING_NAME . " will attempt to fill in the details using the client record associated with this credit note (if applicable), and the default message text.");
-define("INV_EMAIL_SENT_FAILURE_CR", "One or more errors occurred whilst attempting to send e-mail. Please REFRESH THE CREDIT NOTE LIST and then check the e-mail status.");
-define("INV_EMAIL_INVOICE_INTRO_ATTACH_CR", "Dear %s,\n\nThe attached credit note has been generated for you.\n\n");
-define("INV_EMAIL_ATTACH_CR", "Attach HTML credit note?");
+define("INV_NEW_INVOICE_CR", "Ny kredittnote");
+define("INV_INVOICE_DETAILS_CR", "Kredittnote detaljer");
+define("INV_INSTR_INVOICE_NUMBER_CR", "<strong>Husk:</strong> La feltet v&aelig;re blant om du legger til et nytt kredittnotat - komponenten vil automatisk legge til det neste tilgjengelige kredittnote nummer.");
+define("INV_INSTR_BILLING_NAME_CR", "Navnet p&aring; denne personen og/eller firmaet blir refundert.");
+define("INV_BILLING_ADDRESS_CR", "Utbetalingsmottakers adresse");
+define("INV_INVOICE_ITEMS_CR", "Kredittnote elementer");
+define("INV_INSTR_INVOICE_PAID_IN_FULL_CR", "<strong>ADVARSEL!</strong> Denne innstillingen overstyrer den normale prosessen med &aring; lage et 'utgifts' element. Dette er OK hvis du ikke har tenkt &aring; bruke utgifts funksjonen, men det anbefales at du merker kreditnotaer som betales ved &aring; klikke p&aring; den r&oslash;de 'X' p&aring; kredittnotatlisten, snarere enn &aring; sette verdien her.");
+define("INV_INSTR_VENDOR_NAME_CR", "Skriv inn navnet du &oslash;nsker at skal vises p&aring; dine kredittnoter");
+define("INV_INSTR_VENDOR_ADDRESS_CR", "Skriv inn adressen du &oslash;nsker at skal vises p&aring; dine kredittnoter");
+define("INV_EMAIL_INVOICE_SUBJECT_CR", "Kredittnote %s fra %s");
+define("INV_EMAIL_INVOICE_INTRO_CR", "Kjere %s,\n\nDen f&oslash;lgende kredittnoten har blitt generert for deg:");
+define("INV_EMAIL_INVOICE_FOOTER_CR", "Du kan se alle dine fakturaer/kreditnotaer ved &aring; logge inn p&aring; %s. Hvis du ikke lenger &oslash;nsker &aring; motta varsel p&aring; epost, kan du bare logge inn og klikke p&aring; «Min profil` i `Min konto` omr&aring;det &aring; angi dine preferanser.\n\nHilsen,\n %s");
+define("INV_EMAIL_INVOICE_ALT_TEXT_CR", "Du ser denne meldingen fordi din epost klient st&oslash;tter ikke HTML meldinger (eller om du viser det fra s&oslash;ppelpost mappen, HTML kan da bli suspandert. For &aring; se ditt kredittnotat, vennligst se denne eposten i HTML format eller logg inn p&aring; %s for &aring; se dine kredittnotater p&aring; nett.");
+define("INV_EMAIL_INVOICE_NOTIFICATION_CR", "Kjere %s,\n\nDenne eposten skal informere deg om at et nytt kredittnotat er generert for deg.\n\n");
+define("INV_EMAIL_INVOICE_FOOTER_USERNAME_CR", "Du kan se alle dine fakturaer/kredittnotater p&aring; nett ved &aring; logge inn p&aring; %s (ditt brukernavn er %s). Hvis du ikke lenger &oslash;nsker &aring; motta varsel p&aring; epost, kan du bare logge inn og klikke p&aring; «Min profil` i `Min konto` omr&aring;det &aring; angi dine preferanser.\n\nHilsen,\n %s");
+define("INV_SEND_EMAIL_INTRO_CR", "Vennligst velg en epost type for &aring; sende (HTML eller ren tekst). Du kan ogs&aring; overveie denne epost adressen, kontakt navn, og dersom du spesifiserer dine egene verdier her. Dersom dette feltet er blankt, " . INV_BRANDING_NAME . " vil det bli fyllt inn detaljer ved &aring; bruke klient post assosiert med dette kredittnotat (dersom akseptert), og standard meldingstekst.");
+define("INV_EMAIL_SENT_FAILURE_CR", "En eller flere feil oppsto under sending til eposten. Vennligst OPPDATER KREDITTNOTAT LISTEN og sjekk epost statusen.");
+define("INV_EMAIL_INVOICE_INTRO_ATTACH_CR", "Kjere %s,\n\nDet vedlagte kredittnotat har blitt generert for deg.\n\n");
+define("INV_EMAIL_ATTACH_CR", "Legg ved HTML kredittnote?");
 
 //Version 1.2.6
-define("INV_INVOICE_RECORD_LIMIT_WARNING_CR", "WARNING! As there are %s or more clients in your database, only the first %s have been loaded into the above list. If the client you require is not here, please use the 'create new credit note' icon on the client list (the appropriate record will then be selected here automatically).");
+define("INV_INVOICE_RECORD_LIMIT_WARNING_CR", "ADVARSEL! Siden det er %s eller flere kunder i databasen, bare de f&oslash;rste %s har blitt lastet inn i listen ovenfor. Hvis klienten du trenger er ikke her, kan du bruke 'Opprett ny kredittnote'-ikonet p&aring; klientlisten (den aktuelle posten vil da bli valgt her automatisk).");
 ?>
