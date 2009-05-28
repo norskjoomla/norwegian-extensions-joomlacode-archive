b3feb7fd0d516defb2a0c99bb19c1e5228ea21c5
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2009-05-28 22:50:28 +0000

    Fra Roar, fullført

--- utvidelser/nBill/norwegian/configuration.norwegian.php
+++ utvidelser/nBill/norwegian/configuration.norwegian.php
@@ -3,6 +3,7 @@
 * @version 1.1.x
 * @package nBill
 * @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
 * @license Commercial
 * 
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
@@ -21,53 +22,53 @@
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Configuration
-define("INV_CONFIG_TITLE", "Configuration");
-define("INV_CFG_ERROR_EMAIL", "Error e-mail");
-define("INV_CFG_DATE_FORMAT", "Date Format");
-define("INV_CFG_LIST_USERS", "List Users on Client Detail Page");
-define("INV_CFG_INSTR_ERROR_EMAIL", "If any program errors occur, a notification will be sent to this address. NOTE: The e-mail containing the error report may contain potentially sensitive configuration information about your website/server. If you leave this blank, no notification will be sent anywhere. All errors are logged to your database regardless (but error records are deleted from the database after 2 weeks).");
-define("INV_CFG_INSTR_DATE_FORMAT", "Specify the date format to use on invoices (at present, only numeric date formats are supported. d=day, m=month, Y=year eg. UK: \"d/m/Y\"   US: \"m/d/Y\"   International: \"Y/m/d\").");
-define("INV_CONFIG_UPDATED", "The configuration details have been updated.");
-define("INV_CFG_INSTR_LIST_USERS", "Whether or not to display a list of users for you to select from on the Client Details page (to allow you to tie up your clients with user records) - if you have tens of thousands of users, set this to no to avoid a long delay while the page loads (if set to no, you can still specify a user record by typing in the user id directly).");
-define("INV_CFG_DATABASE_FUNCTIONS", "Database Functions");
-define("INV_CFG_INSTR_DATABASE_FUNCTIONS", "Clear down or delete " . INV_BRANDING_NAME . " database tables. Clearing down the tables will delete all of your data, and allow you to start with a clean slate (you will need to re-enter your license key). Delete tables before uninstalling the component to completely remove the component from your system. DO NOT delete the tables if you want to upgrade to the latest version of " . INV_BRANDING_NAME . " and keep your data! NOTE: DELETING THE TABLES WILL MAKE " . strtoupper(INV_BRANDING_NAME) . " INOPERABLE. After deleting, you must uninstall the component as it will no longer work.");
-define("INV_CFG_CLEAR_TABLES", "Clear Down Tables");
-define("INV_CFG_DELETE_TABLES", "Delete Tables");
-define("INV_CFG_CONFIRM_CLEAR", "Are you sure you want to permanently delete all of your data? (if you go ahead, you will need to re-enter your license key)");
-define("INV_CFG_CONFIRM_DELETE", "WARNING! This will stop " . INV_BRANDING_NAME . " from working! Are you sure you want to permanently DELETE all of the " . INV_BRANDING_NAME . " tables?");
-define("INV_CFG_TABLES_CLEARED", "Tables Cleared. All of your data has been deleted. You will have to re-enter your license key to continue using " . INV_BRANDING_NAME . ".");
-define("INV_CFG_TABLES_DELETED", "All " . INV_BRANDING_NAME . " tables have been deleted. To complete uninstallation, please refer to the %s");
-define("INV_CFG_UNINSTALLER", "Component Installer");
-define("INV_CFG_CRON_TOKEN", "CRON authorisation token");
-define("INV_CFG_INSTR_CRON_TOKEN", "Enter any word here (no punctuation please!) and set the same word in your CRON script to give the script authorisation to run (eg. for automatically generating invoices on a daily basis). This is to prevent unauthorised scripts from accessing " . INV_BRANDING_NAME . ". See " . INV_BRANDING_WEBSITE . " for more information.");
-define("INV_CFG_LICENSE_KEY", "License Key");
-define("INV_CFG_INSTR_LICENSE_KEY", "If you wish to update your license key before it expires, you can enter a new one here. WARNING! If you enter an incorrect value here, it could prevent you from using " . INV_BRANDING_NAME . "! Only enter a valid license key, and copy it EXACTLY. By entering a license key, you are confirming that you understand and accept the %s");
-define("INV_CFG_EULA", "End User License Agreement");
-define("INV_CFG_VERSION_AUTO_CHECK", "Check for new versions?");
-define("INV_CFG_AUTO_UPDATE", "Automatically upgrade?");
-define("INV_CFG_INSTR_VERSION_AUTO_CHECK", "Whether or not to automatically check for a new version of the component whenever you load the admin home page. No personal information is sent anywhere.");
-define("INV_CFG_INSTR_AUTO_UPDATE", "Whether or not to automatically upgrade your software if possible whenever a new version is released. Only applicable if 'Check for new versions' is set to 'yes'. If this option is set to 'no', you will just be prompted on the home page that a new version is available, but the new version will not be downloaded or installed.");
-define("INV_CFG_CHECK_VERSION", "Check for Updates");
-define("INV_CFG_INSTR_CHECK_VERSION", "Click the button to check now whether a newer version of the component is available.");
-define("INV_CFG_BTN_CHECK_VERSION", "Check Now");
-define("INV_CFG_UPDATE_NOW", "Upgrade Component");
-define("INV_CFG_INSTR_UPDATE_NOW", "Click the button to upgrade to the latest version now (if applicable).");
-define("INV_CFG_BTN_UPDATE_NOW", "Upgrade Now");
+define("INV_CONFIG_TITLE", "Konfigurasjon");
+define("INV_CFG_ERROR_EMAIL", "E-post ved feil");
+define("INV_CFG_DATE_FORMAT", "Dato format");
+define("INV_CFG_LIST_USERS", "Liste opp brukere på klient siden");
+define("INV_CFG_INSTR_ERROR_EMAIL", "Om det oppstår feil med programmet, vil en melding bli sendt til denne e-postadressen. MERK: Meldingen som sendes kan inneholde sensitive konfigurasjonsdata om din nettside/server. Om feltet er blankt vil det IKKE bli sendt noe melding ved feil. Alle feil blir lagret i databasen (slettes etter 2 uker).");
+define("INV_CFG_INSTR_DATE_FORMAT", "Fastsette datoformatet som skal brukes på fakturaen (for øyeblikket kan dato bare angis med tall. d=dag, m=måned, Y=år f. eks. NO: \"d/m/Y\"   US: \"m/d/Y\"   Internasjonalt: \"Y/m/d\").");
+define("INV_CONFIG_UPDATED", "Konfigurasjonen er oppdatert.");
+define("INV_CFG_INSTR_LIST_USERS", "Hvorvidt liste over brukere, som du kan velge fra, skal vises på kliensiden (så du kan koble klient og brukerdata) - har du tusenvis av bruker, så sett dette til NEI så lastes siden raskere (velger du nei, kan du forsatt koble bruker med klient ved å skrive inn bruker ID direkte).");
+define("INV_CFG_DATABASE_FUNCTIONS", "Databasefunksjoner");
+define("INV_CFG_INSTR_DATABASE_FUNCTIONS", "Komprimer eller slett " . INV_BRANDING_NAME . " tabellene. Komprimering av tabellene vil slette all data så du kan starte på nytt igjen (du må angi lisenskoden på nytt). For å kunne slette komponenten fullstendig må du først slette tabellene før du avinstallere komponenten. IKKE slett tabellene om du skal oppgradere til siste versjon av " . INV_BRANDING_NAME . " og beholde dataene! MERK: SLETTING AV TABELLENE GJØR " . strtoupper(INV_BRANDING_NAME) . " UTE AV FUNKJON. Etter sletting bør du avinstallere komponenten, den virker ikke mer!");
+define("INV_CFG_CLEAR_TABLES", "Komprimer tabellene");
+define("INV_CFG_DELETE_TABLES", "Slett tabellene");
+define("INV_CFG_CONFIRM_CLEAR", "Er du sikker på at du vil slette all data? Du kan IKKE gjenopprettes! (om du fortsetter må du angi lisenskoden på nytt)");
+define("INV_CFG_CONFIRM_DELETE", "ADVARSEL! På denne måte vil " . INV_BRANDING_NAME . " ikke virke mer! Er du sikker på at du vil SLETTE alle " . INV_BRANDING_NAME . " tabellene permanent?");
+define("INV_CFG_TABLES_CLEARED", "Tabellene slettet. Alle dine data er slettet. Du må du angi lisenskoden på nytt for å fortsatt kunne bruke " . INV_BRANDING_NAME . ".");
+define("INV_CFG_TABLES_DELETED", "Alle " . INV_BRANDING_NAME . " tabellene er slettet. For å fullstendig avinstaller, se %s");
+define("INV_CFG_UNINSTALLER", "Komponent installert");
+define("INV_CFG_CRON_TOKEN", "CRON autorisasjons kode");
+define("INV_CFG_INSTR_CRON_TOKEN", "Skriv inn en kode her (ingen skilletegn takk!) og skriv inn den samme koden i så CRON skriptet kan kjøres (f. eks. til daglig automatisk generering av fakturaer ). Dette for å hindre at ikke godkjente skript kjøres på " . INV_BRANDING_NAME . ". Se " . INV_BRANDING_WEBSITE . " for mer informasjon.");
+define("INV_CFG_LICENSE_KEY", "Lisenskode");
+define("INV_CFG_INSTR_LICENSE_KEY", "Ønsker du å oppdatere din lisenskode før den utløper kan du skrive inne den ny koden her. ADVARSEL! Skriver du feil kode kan de gjøre at du ikke kan bruke " . INV_BRANDING_NAME . "! Skriv inn en gyldig lisenskode og NØYAKTIG slik den er. Ved å skrive inn lisenskoden, bekrefter du, godtar og forstår %s");
+define("INV_CFG_EULA", "Avslutt avtalen");
+define("INV_CFG_VERSION_AUTO_CHECK", "Se etter nye versjoner?");
+define("INV_CFG_AUTO_UPDATE", "Oppgrader automatisk?");
+define("INV_CFG_INSTR_VERSION_AUTO_CHECK", "Om du automatisk skal eller ikke skal sjekke for nyere versjoner hver gang administrator siden. Ingen personlige data blir sendt.");
+define("INV_CFG_INSTR_AUTO_UPDATE", "Om du automatisk skal eller ikke skal oppgradere programvaren når nyere versjoner er tilgjenelig. Gjelder bare om 'Se etter nye versjoner' er satt til 'ja'. Er dette valget satt til 'nei', får du bare en påminnelse om at ny versjon er tilgjengelig, men den nye versjonen vil IKKE bli lastet ned og installert.");
+define("INV_CFG_CHECK_VERSION", "Se etter oppdateringer");
+define("INV_CFG_INSTR_CHECK_VERSION", "Klikk på denne får å sjekke om en nyere versjon av komponenten finnes.");
+define("INV_CFG_BTN_CHECK_VERSION", "Sjekk nå");
+define("INV_CFG_UPDATE_NOW", "Oppgrader komponenten");
+define("INV_CFG_INSTR_UPDATE_NOW", "Klikk på knappen for å oppgradere til nyest versjon (om gjeldende).");
+define("INV_CFG_BTN_UPDATE_NOW", "Oppgrader nå");
 
 /* Version 1.1.4 */
-define("INV_CFG_LIST_START_DATE", "Default Start Date for Lists");
-define("INV_CFG_INSTR_LIST_START_DATE", "Indicate how you want the system to select the start date for any lists that are governed by a date range (eg. orders, invoices) when no date has been specifically selected. WARNING! If you select to show a large number of records (eg. 5 years, or all), this could slow down the display of the lists.");
-define("INV_CFG_START_DATE_CURRENT_ONLY", "Show current month only");
-define("INV_CFG_START_DATE_CURRENT_LAST", "Show current and previous month");
-define("INV_CFG_START_DATE_QUARTER", "Show up to 3 months");
-define("INV_CFG_START_DATE_SEMI", "Show up to 6 months");
-define("INV_CFG_START_DATE_YEAR", "Show up to a year");
-define("INV_CFG_START_DATE_FIVE", "Show up to 5 years");
-define("INV_CFG_START_DATE_ALL", "Show ALL items");
+define("INV_CFG_LIST_START_DATE", "Standard startdatolister");
+define("INV_CFG_INSTR_LIST_START_DATE", "Indikerer hvordan du vil at systemet skal velge startdato for lister som bruke datoområde (f. eks. ordre, faktura) når ingen dato valgt. ADVARSEL! Velger du f. eks å vise poster for de siste 5 årene kan dette gjøre at det tar tid å vise listen.");
+define("INV_CFG_START_DATE_CURRENT_ONLY", "Vis bare denne måned");
+define("INV_CFG_START_DATE_CURRENT_LAST", "Vis denne og forrige måned");
+define("INV_CFG_START_DATE_QUARTER", "Vis opp til 3 måneder");
+define("INV_CFG_START_DATE_SEMI", "Vis opp til 6 måneder");
+define("INV_CFG_START_DATE_YEAR", "Vis opp til ett år");
+define("INV_CFG_START_DATE_FIVE", "Vis opp til 5 år");
+define("INV_CFG_START_DATE_ALL", "Vis ALLE postene");
 
 //Version 1.2.1
-define("INV_CFG_SWITCH_TO_SSL", "Switch to SSL on login?");
-define("INV_CFG_INSTR_SWITCH_TO_SSL", "Whether or not to switch to https after a user logs in using any login box within " . INV_BRANDING_NAME . " (will only affect login boxes that are output by " . INV_BRANDING_NAME . "). WARNING! You must have an SSL certificate installed for this to work, and it may require hacks to your CMS (See article on <a target=\"blank\" href=\"http://www.netshinesoftware.com/security/using-an-ssl-certificate-with-your-joomla-website.html\">Netshine Software website</a>).");
+define("INV_CFG_SWITCH_TO_SSL", "Bytt til SSL ved innlogging?");
+define("INV_CFG_INSTR_SWITCH_TO_SSL", "Om du skal eller ikke skal bytte til https etter at brukeren har logget inn ved bruk av logg inn fra " . INV_BRANDING_NAME . " (vil bare gjelde logg inn fra  " . INV_BRANDING_NAME . "). ADVARSEL! Du må ha installert et SSL sertifikat for at dette skal virke. og det kan hende du må modifisere filer (Se artikkel på <a target=\"blank\" href=\"http://www.netshinesoftware.com/security/using-an-ssl-certificate-with-your-joomla-website.html\">Netshine Software website</a>).");
 
 //Version 1.2.3 - Note to translators:
 //Line 29 of original English language file amended (INV_CFG_INSTR_DATE_FORMAT) - date formats currently restricted to numeric
