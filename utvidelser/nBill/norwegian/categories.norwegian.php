e4731001e777936e2a5992cf59ec9abf9a62f040
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-05-28 19:37:16 +0000

    Fra Kristian, fullført.

--- utvidelser/nBill/norwegian/categories.norwegian.php
+++ utvidelser/nBill/norwegian/categories.norwegian.php
@@ -3,6 +3,7 @@
 * @version 1.1.x
 * @package nBill
 * @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
 * @license Commercial
 * 
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
@@ -21,17 +22,17 @@
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Categories
-define("INV_CATEGORIES_TITLE", "Categories");
-define("INV_CATEGORY_NAME", "Name");
-define("INV_CATEGORY_DESCRIPTION", "Description");
-define("INV_CATEGORY_ORDERING", "Ordering");
-define("INV_EDIT_CATEGORY", "Edit Category");
-define("INV_NEW_CATEGORY", "New Category");
-define("INV_ERR_CANNOT_DELETE_CATEGORY", "One or more Categories you tried to delete is not empty. You cannot delete a Category which contains Products or other Categories. Please either delete or move the Products and/or child Categories to a different Category before attempting to delete.");
-define("INV_ERR_CANNOT_DELETE_ROOT_CAT", "You cannot delete the root category - there must always be at least one category present.");
-define("INV_CATEGORY_NAME_REQUIRED", "Please enter a name for the Category");
-define("INV_CATEGORY_DETAILS", "Category Details");
-define("INV_CATEGORY_PARENT", "Parent Category");
+define("INV_CATEGORIES_TITLE", "Kategorier");
+define("INV_CATEGORY_NAME", "Navn");
+define("INV_CATEGORY_DESCRIPTION", "Beskrivelse");
+define("INV_CATEGORY_ORDERING", "Sortering");
+define("INV_EDIT_CATEGORY", "Rediger kategori");
+define("INV_NEW_CATEGORY", "Ny kategori");
+define("INV_ERR_CANNOT_DELETE_CATEGORY", "Én eller flere kategorier du prøvde å slette ikke er tom. Du kan ikke slette en kategori som inneholder produkter eller andre kategorier. Du må enten slette eller flytte de produktene og / eller underkategorien til en annen kategori før du prøver å slette.");
+define("INV_ERR_CANNOT_DELETE_ROOT_CAT", "Du kan ikke slette hovedkategorien (rot) - det må alltid finnes minst én kategori.");
+define("INV_CATEGORY_NAME_REQUIRED", "Skriv inn navnet for kategorien");
+define("INV_CATEGORY_DETAILS", "Kategoridetaljer");
+define("INV_CATEGORY_PARENT", "Overordnet kategori");
 define("INV_INSTR_CATEGORY_NAME", "");
 define("INV_INSTR_CATEGORY_DESCRIPTION", "");
 define("INV_INSTR_CATEGORY_PARENT", "");
