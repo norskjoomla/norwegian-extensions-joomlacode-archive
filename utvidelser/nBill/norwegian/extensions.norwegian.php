c56320897ee04da7e15b0d10aebdd7e25a550af9
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-05-30 19:59:20 +0000

    Språkvasket og fullført - Rune

--- utvidelser/nBill/norwegian/extensions.norwegian.php
+++ utvidelser/nBill/norwegian/extensions.norwegian.php
@@ -3,6 +3,7 @@
 * @version 1.1.x
 * @package nBill
 * @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
 * @license Commercial
 * 
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
@@ -21,32 +22,32 @@
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Extensions
-define("INV_EXTENSION_INSTALL_WARNING", "ADVARSEL! Bare innstaller utvidelser fra kilder du stoler på. En utvidelse kan få tilgang til hele webområdet!");
-define("INV_EXTENSION_NO_GATEWAY_FILE", "Ingen fil er markert som betalingsmåtefil");
-define("INV_EXTENSION_NO_LANGUAGE_FILE", "Ingen fil er markert som språkfil");
-define("INV_EXTENSION_INSTALL_NEW", "Innstaller ny utvidelse");
-define("INV_EXTENSIONS_INSTALLED", "Innstallerte utvidelser");
-define("INV_EXTENSION_NAME", "Utvidelses navn");
+define("INV_EXTENSION_INSTALL_WARNING", "ADVARSEL! Bare innstaller utvidelser fra kilder du stoler på. En utvidelse kan få tilgang til hele nettsiden din!");
+define("INV_EXTENSION_NO_GATEWAY_FILE", "Ingen fil er merket som en fil for betalingsmodul");
+define("INV_EXTENSION_NO_LANGUAGE_FILE", "Ingen fil er merket som en språkfil");
+define("INV_EXTENSION_INSTALL_NEW", "Installér ny utvidelse");
+define("INV_EXTENSIONS_INSTALLED", "Installerte utvidelser");
+define("INV_EXTENSION_NAME", "Utvidelsens navn");
 define("INV_EXTENSION_TYPE", "Type");
-define("INV_EXTENSION_DATE_CREATED", "Opprettet dato");
-define("INV_EXTENSION_DATE_INSTALLED", "Innstallert dato");
+define("INV_EXTENSION_DATE_CREATED", "Produksjondato");
+define("INV_EXTENSION_DATE_INSTALLED", "Installasjonsdato");
 define("INV_EXTENSION_VERSION", "Versjon");
-define("INV_EXTENSION_AUTHOR", "Skribrent");
-define("INV_EXTENSION_URL", "Adresse");
-define("INV_EXTENSION_UNINSTALL", "Avinnstaller");
+define("INV_EXTENSION_AUTHOR", "Utvikler");
+define("INV_EXTENSION_URL", "Nettadresse");
+define("INV_EXTENSION_UNINSTALL", "Avinstaller");
 define("INV_EXTENSION_NOT_REMOVED", "Beklager, " . INV_BRANDING_NAME . " kunne ikke slette utvidelsen '%s'. Den må fjernes manuelt.");
-define("INV_EXTENSION_REMOVED", "Utvidelsen '%s' ble avinnstallert med suksess.");
-define("INV_UNINSTALL_KEEP_SETTINGS", "Do you want to keep your settings, so you can upgrade the extension (if applicable)? Select `OK` to leave any database settings intact, or `Cancel` to permanently delete this extension.");
+define("INV_EXTENSION_REMOVED", "Utvidelsen '%s' ble vellykket avinstallert.");
+define("INV_UNINSTALL_KEEP_SETTINGS", "Vil du beholde dine innstillinger, slik at du kan oppdatere utvidelsen (om det er aktuelt)? Velg `OK` for å la database være uberørt, eller `Avbryt` for å slette denne utvidelsen for godt.");
 
 //Version 1.2.0
-define("INV_EXTENSION_NO_INSTALL_FILE", "No install file (*.nbe) was found. This extension cannot be installed. Please make sure you are using the correct version of the extension (extensions written for older versions of " . INV_BRANDING_NAME . " might not work with this version)");
-define("INV_EXTENSION_INVALID_INSTALL_FILE", "The install file (%s) is not a valid nBill extension installation file. This extension cannot be installed.");
-define("INV_EXTENSION_COULD_NOT_CREATE_DIR", "The installation failed because the following directory could not be created: %s (if this directory already exists, try uninstalling the existing extension first).");
-define("INV_EXTENSION_COULD_NOT_CREATE_FILE", "The installation failed because the following file could not be created: %s (if this file already exists, try uninstalling the existing extension first).");
-define("INV_EXTENSION_INSTALLED", "Extension '%s' installed successfully!");
-define("INV_EXTENSION_RETURN_TO_LIST", "to return to the extension installer");
-define("INV_EXTENSION_VERSION_INCOMPATIBLE", "The version of " . INV_BRANDING_NAME . " that you are using is not compatible with the extension you are trying to install");
+define("INV_EXTENSION_NO_INSTALL_FILE", "Ingen installasjonsfil (*.nbe) ble funnet. Utvidelsen kan ikke installeres. Forsikre deg om at du bruker riktig versjon av utvidelsen (utvidelser skrevet for eldre versjoner av " . INV_BRANDING_NAME . " vil kanskje ikke virke med denne versjonen).");
+define("INV_EXTENSION_INVALID_INSTALL_FILE", "Installasjonsfilen (%s) er ikke en gyldig nBill fil. Utvidelsen kan ikke installeres.");
+define("INV_EXTENSION_COULD_NOT_CREATE_DIR", "Installasjonen feilet fordi følgende mappe ikke kunne opprettes: %s (om denne mappen allerede eksisterer, forsøk da å avinstallere den eksisterende utvidelsen først).");
+define("INV_EXTENSION_COULD_NOT_CREATE_FILE", "Installasjonen feilet fordi følgende fil ikke kunne opprettes: %s (om denne filen allerede eksisterer, forsøk da å avinstallere den eksisterende utvidelsen først).");
+define("INV_EXTENSION_INSTALLED", "Utvidelsen '%s' ble vellykket installert!");
+define("INV_EXTENSION_RETURN_TO_LIST", "for å returnere til utvidelsesinstallasjonen");
+define("INV_EXTENSION_VERSION_INCOMPATIBLE", "Versjonen av " . INV_BRANDING_NAME . " som du bruker er ikke kompatibel med utvidelsen du forsøker å installere");
 
 //Version 1.2.8
-define("INV_EXTENSION_DB_ERROR", "The installation might not have completed correctly because the following database error occurred: %s");
+define("INV_EXTENSION_DB_ERROR", "Installasjonen ble kanskje ikke vellykket fordi følgende databasefeil oppsto: %s");
 ?>
