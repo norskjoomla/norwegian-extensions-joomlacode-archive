64508a80e9fa8230d9a9ba2ff8c31f5e62107fa1
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-06-06 12:08:31 +0000

    Fra Roar, fullført

--- utvidelser/nBill/norwegian/reminders.norwegian.php
+++ utvidelser/nBill/norwegian/reminders.norwegian.php
@@ -22,75 +22,75 @@
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Reminders
-define("INV_REMINDERS_TITLE", "Reminders");
-define("INV_REMINDERS_WARNING", "Warning! This feature has the potential to send e-mails to ALL of your clients. Please be careful, and use at your own risk!");
-define("INV_REMINDERS_INTRO", "Record details here of any reminders you want to send to yourself or to clients when payments are due, orders are up for renewal, or about to expire, or when invoices are overdue. PLEASE NOTE: Setting these values will have NO EFFECT UNLESS you have also set up a scheduled task to actually check what is due and fire off the e-mails. To do this, you would typically use a CRON job along with the scheduled reminder script - available from the " . INV_BRANDING_NAME . " website. Please see the documentation for more details on how to set up a CRON job.");
-define("INV_REMINDER_NAME", "Reminder Name");
-define("INV_EDIT_REMINDER", "Edit Reminder");
-define("INV_REMINDER_TYPE", "Reminder Type");
-define("INV_REMINDER_ACTIVE", "Active?");
-define("INV_REMINDER_ACTIVE_YES", "Active");
-define("INV_REMINDER_ACTIVE_NO", "Inactive");
-define("INV_REMINDER_NAME_REQUIRED", "Please enter a name to identify this reminder");
-define("INV_REMINDER_STARTING_FROM", "Starting From");
-define("INV_REMINDER_NUMBER_OF_UNITS", "Number of Units");
-define("INV_NEW_REMINDER", "New Reminder");
-define("INV_REMINDER_DETAILS", "Reminder Details");
-define("INV_INSTR_REMINDER_NAME", "Enter a name that will help you easily identify what this reminder is for.");
-define("INV_INSTR_REMINDER_TYPE", "Select the type of reminder you want to set up.");
-define("INV_INSTR_REMINDER_ACTIVE", "Whether or not to send e-mails out for this reminder.");
+define("INV_REMINDERS_TITLE", "Påminnelser");
+define("INV_REMINDERS_WARNING", "Denne funksjonen har potensial til å sende e-post til ALLE dine kunder. Vær forsiktig, og bruk skjer på eget ansvar!");
+define("INV_REMINDERS_INTRO", "Dine påminnelser som du vil sende til deg selv eller til dine kunder når utbetalingene forfaller eller ordre skal fornyes, eller er i ferd med å utløpe, eller når fakturaer ikke er betalt. MERK: Disse innstillingene har INGEN effekt med mindre du også har satt opp en planlagt oppgave til å sjekke hva som er forfalt og så sende en e-post. For å gjøre dette vil du vanligvis bruke en CRON jobb sammen med ett skript, planlagt påminnelse - som er tilgjengelig fra " . INV_BRANDING_NAME . " nettside. Vennligst se dokumentasjonen for mer informasjon om hvordan du setter opp en CRON jobb.");
+define("INV_REMINDER_NAME", "Påminnelsesnavn");
+define("INV_EDIT_REMINDER", "Rediger påminnelse");
+define("INV_REMINDER_TYPE", "Type påminnelse");
+define("INV_REMINDER_ACTIVE", "Aktiv?");
+define("INV_REMINDER_ACTIVE_YES", "Aktive");
+define("INV_REMINDER_ACTIVE_NO", "Inaktiv");
+define("INV_REMINDER_NAME_REQUIRED", "Skriv inn et navn for å identifisere denne påminnelsen");
+define("INV_REMINDER_STARTING_FROM", "Fra");
+define("INV_REMINDER_NUMBER_OF_UNITS", "Antall enheter");
+define("INV_NEW_REMINDER", "Ny påminnelse");
+define("INV_REMINDER_DETAILS", "Påminnelsesdetaljer");
+define("INV_INSTR_REMINDER_NAME", "Skriv inn et navn som gjør det lett å identifisere hva denne påminnelsen gjelder.");
+define("INV_INSTR_REMINDER_TYPE", "Velg type påminnelse du vil lage.");
+define("INV_INSTR_REMINDER_ACTIVE", "Hvorvidt om det skal sendes e-post eller ikke for denne påminnelsen.");
 define("INV_REMINDER_ADMIN", "Administrator?");
-define("INV_INSTR_REMINDER_ADMIN", "Whether or not to send the reminder e-mail to the administrator rather than the client (if you want a reminder sent to both administrator and client, please set up 2 separate reminder records).");
-define("INV_INSTR_REMINDER_STARTING_FROM", "Date from which to begin sending reminder e-mails (if blank, reminders will be sent regardless of date).");
-define("INV_REMINDER_NO_OF_UNITS", "Number of Units");
-define("INV_INSTR_REMINDER_NO_OF_UNITS", "Enter the number of units for the interval between the event and the reminder (or in the case of user-defined reminders, the interval between reminders). For example, for an interval of 7 days, the number of units is 7 (and the units are 'Days').");
-define("INV_REMINDER_UNITS", "Units");
-define("INV_INSTR_REMINDER_UNITS", "Select the type of units for the interval.");
-define("INV_REMINDER_SEND_AFTER", "Send After?");
-define("INV_INSTR_REMINDER_SEND_AFTER", "Whether or not to send the reminder AFTER the event (typically only used for overdue invoices - ie. to send a reminder x days after the invoice date).");
-define("INV_REMINDER_EMAIL_TEXT", "E-Mail Text");
-define("INV_INSTR_REMINDER_EMAIL_TEXT", "Type in the e-mail message you want to send. You can use the following tokens: {NAME} = inserts client name; {VENDOR} = inserts vendor name; {DATE} = inserts event date; {INTERVAL} = inserts number of units; {UNITS} = inserts type of unit; {ORDER} = order details; {AMOUNT} = amount owing (nb. discounts will NOT be taken into account); {WEBSITE} = your website address; {PAYLINK} = inserts a payment link, if applicable (renewals and overdue invoices only); {PAYLINK_HTTPS} = same as paylink, but uses https at the start of the URL; {INVOICE} = inserts invoice number (overdue invoices only).");
-define("INV_REMINDER_CLIENT", "Client");
-define("INV_INSTR_REMINDER_CLIENT", "The client to whom the reminder should be sent (if 'Administrator' setting above is set to 'No'). WARNING! If this is set to 'Not Applicable', a reminder will be sent to EVERY client for whom it is due. If this is a 'user-defined' reminder, and the 'Administrator' setting is set to 'no', you MUST select a client here, otherwise no reminder will be sent.");
+define("INV_INSTR_REMINDER_ADMIN", "Hvorvidt påminnelsen skal sendes til administrator og ikke til kunden (hvis du ønsker påminnelse sendt til både administrator og kunde, må du sette opp 2 separate påminnelser).");
+define("INV_INSTR_REMINDER_STARTING_FROM", "Dato for når å begynne å sende påminnelses e-post (hvis tomt, vil påminnelse bli sendt uavhengig av dato).");
+define("INV_REMINDER_NO_OF_UNITS", "Antall enheter");
+define("INV_INSTR_REMINDER_NO_OF_UNITS", "Angi antall enheter for intervallet mellom hendelsen og påminnelsen (eller i tilfelle av brukerdefinerte påminnelser, intervallet mellom påminnelser). F. eks. til et intervall av 7 dager, er tallet 7 (og enheter er 'Dager').");
+define("INV_REMINDER_UNITS", "Enhet");
+define("INV_INSTR_REMINDER_UNITS", "Velg type enhet for dette intervallet.");
+define("INV_REMINDER_SEND_AFTER", "Send etter?");
+define("INV_INSTR_REMINDER_SEND_AFTER", "Hvorvidt å sende påminnelse etter hendelsen eller ikke (vanligvis bare bruket på forfalte fakturaer - dvs. å sende en påminnelse x dager etter forfallsdato på faktura).");
+define("INV_REMINDER_EMAIL_TEXT", "E-post tekst");
+define("INV_INSTR_REMINDER_EMAIL_TEXT", "Skriv inn e-postmeldingen du ønsker å sende. Du kan bruke følgende frase: {NAME} = setter inn kundens navn; {VENDOR} = setter inn navn på leverandør; {DATE} = setter inn dato; {INTERVAL} = setter inn antall; {UNITS} = setter inn enhet; {ORDER} = ordredetaljer; {AMOUNT} = utestående beløp  (NB. rabatter vil IKKE bli tatt med); {WEBSITE} = din nettsides adresse; {PAYLINK} = setter inn en betalingslink, hvis det er aktuelt (fornyelser og forfalte fakturaer bare); {PAYLINK_HTTPS} = samme som 'paylink', men bruker https i starten av nettadressen; {INVOICE} = setter inn fakturanr. (bare for forfalte fakturaer).");
+define("INV_REMINDER_CLIENT", "Kunde");
+define("INV_INSTR_REMINDER_CLIENT", "Kunde som påminnelsen skal sendes til (hvis til 'Administrator' - settes innstillingen ovenfor til 'Nei'). ADVARSEL! Hvis dette er satt til 'Gjelder ikke', vil en påminnelse bli sendt til ALLE kundene som har ett forfall. Hvis dette er en 'brukerdefinert' påminnelse, og 'Administrator' innstillingen er satt til 'Nei', må du velge en kunde her, ellers vil ingen påminnelse blir sendt.");
 define("INV_REMINDER_FILTER", "Filter");
-define("INV_INSTR_REMINDER_FILTER", "FOR ADVANCED USERS ONLY: You can filter the records to which the reminder will apply by entering a filter here in the format of an SQL 'where' clause. For example, to send payment due reminders only to clients with orders that have annual payment intervals, you could enter \" #__inv_orders.payment_frequency = 'EE' \" here. The database tables on which filtering can be performed depend on the type of reminder.");
+define("INV_INSTR_REMINDER_FILTER", "BARE FOR AVANSERTE BRUKERE: Du kan filtrere postene som påminnelsen vil gjelde for ved å legge inn et filter her i form av en SQL 'where' setning. F. eks, å sende påminnelser bare til kunder med bestillinger som har årlige betalings intervaller, Du kan legge inn \" #__inv_orders.payment_frequency = 'EE' \" her. Databasetabeller som filtrering kan utføres på avhenger av typen påminnelse.");
 
 //Default e-mail text
-define("INV_REMINDER_PAYMENT_DUE_EMAIL", "Dear {NAME},\n\nThis is a courtesy e-mail to let you know that a recurring payment for the following order is due on {DATE}. If you have set up a recurring payment schedule, you will be automatically debited with {AMOUNT} (less any discounts for which you qualify, if applicable) on or around the due date.\n\nOrder Details:\n\n{ORDER}\n\nIf you no longer wish to receive these reminders, please login to your account at {WEBSITE}, and set your preferences under 'My Account->My Profile'.\n\nRegards,\n{VENDOR}");
-define("INV_REMINDER_ORDER_EXPIRY_EMAIL", "Dear {NAME},\n\nThis is a courtesy e-mail to let you know that the following order will expire on {DATE}. Please ensure any regular payment schedule is cancelled.\n\nOrder Details:\n\n{ORDER}\n\nIf you no longer wish to receive these reminders, please login to your account at {WEBSITE}, and set your preferences under 'My Account->My Profile'.\n\nRegards,\n{VENDOR}.");
-define("INV_REMINDER_RENEWAL_DUE_EMAIL", "Dear {NAME},\n\nThis is a reminder that your subscription for the following order is due for renewal on {DATE}. If you would like to renew, please use the following link: {PAYLINK}. Any remaining time on your old subscription will be added to the new one so you will not lose out by renewing early.\n\n{ORDER}\n\nIf you no longer wish to receive these reminders, please login to your account at {WEBSITE}, and set your preferences under 'My Account->My Profile'.\n\nRegards,\n{VENDOR}");
-define("INV_REMINDER_INVOICE_OVERDUE_EMAIL", "Dear {NAME},\n\nThis is a reminder that invoice number {INVOICE} for {AMOUNT} has been outstanding for {INTERVAL} {UNITS}, and is therefore now overdue. Please arrange to pay this invoice as soon as possible. If you would like to pay online, you can use the following link: {PAYLINK}.\n\nRegards,\n{VENDOR}.");
-define("INV_REMINDER_USER_DEFINED_EMAIL", "Don't forget: <Enter reminder text here!>");
-define("INV_REMINDER_PAYMENT_DUE_SUBJECT", "Reminder: Payment Due");
-define("INV_REMINDER_ORDER_EXPIRY_SUBJECT", "Reminder: Order Expiry");
-define("INV_REMINDER_RENEWAL_DUE_SUBJECT", "Reminder: Renewal Due");
-define("INV_REMINDER_INVOICE_OVERDUE_SUBJECT", "Reminder: Invoice Overdue");
-define("INV_REMINDER_USER_DEFINED_SUBJECT", "Reminder");
+define("INV_REMINDER_PAYMENT_DUE_EMAIL", "Hei {NAME},\n\nDette er en e-post for å meddele at en periodisk betaling for følgende ordre forfaller den {DATE}. Hvis du har angitt en periodisk betaling, vil du automatisk bli belastet med {AMOUNT} (fratrukket eventuelle rabatter om du er kvalifisert til det) på eller rundt forfallsdato.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger ønsker å motta disse påminnelsene, må du logge inn på din konto på {WEBSITE}, og endre innstillinger under ’Min konto-> Min profil'.\n\nHilsen,\n{VENDOR}");
+define("INV_REMINDER_ORDER_EXPIRY_EMAIL", "Hei {NAME},\n\nDette er en e-post for å meddele at følgende ordre utløper {DATE}. Påse at eventuelle betalingsplaner er annullert.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger ønsker å motta disse påminnelsene, må du logge inn på din konto på {WEBSITE}, og endre innstillinger under ’Min konto-> Min profil'.\n\nHilsen,\n{VENDOR}.");
+define("INV_REMINDER_RENEWAL_DUE_EMAIL", "Hei {NAME},\n\nDette er en påminnelse om at abonnementet for følgende ordre forfaller på {DATE}. Hvis du ønsker å fornye, kan du bruke følgende link: {PAYLINK}. Eventuell gjenværende tid på det gamle abonnementet vil bli lagt til på det nye slik at du ikke taper ved å fornye tidligere.\n\n{ORDER}\n\nHvis du ikke lenger ønsker å motta disse påminnelsene, må du logge inn på din konto på {WEBSITE}, og endre innstillinger under ’Min konto-> Min profil'.\n\nHilsen,\n{VENDOR}");
+define("INV_REMINDER_INVOICE_OVERDUE_EMAIL", "Hei {NAME},\n\nDette er en påminnelse om at fakturanummer {INVOICE} for {AMOUNT} har vært utestående for {INTERVAL} {UNITS}, og er derfor nå forfalt. Vennligst betal denne fakturaen så snart som mulig. Hvis du ønsker å betale elektronisk, kan du bruke følgende link: {PAYLINK}.\n\nHilsen,\n{VENDOR}.");
+define("INV_REMINDER_USER_DEFINED_EMAIL", "Ikke glemt: <Skriv inn tekst her!>");
+define("INV_REMINDER_PAYMENT_DUE_SUBJECT", "Påminnelse: Betaling forfalt");
+define("INV_REMINDER_ORDER_EXPIRY_SUBJECT", "Påminnelse: Ordre utløpt");
+define("INV_REMINDER_RENEWAL_DUE_SUBJECT", "Påminnelse: Fornyelse forfalt");
+define("INV_REMINDER_INVOICE_OVERDUE_SUBJECT", "Påminnelse: Faktura forfalt");
+define("INV_REMINDER_USER_DEFINED_SUBJECT", "Påminnelse");
 
 //Reminder Types
-define("INV_REMINDER_PAYMENT_DUE", "Payment Due");
-define("INV_REMINDER_ORDER_EXPIRY", "Order Expiry");
-define("INV_REMINDER_RENEWAL_DUE", "Renewal Due");
-define("INV_REMINDER_INVOICE_OVERDUE", "Invoice Overdue");
-define("INV_REMINDER_USER_DEFINED", "User-defined");
+define("INV_REMINDER_PAYMENT_DUE", "Betaling forfalt");
+define("INV_REMINDER_ORDER_EXPIRY", "Ordre utløpt");
+define("INV_REMINDER_RENEWAL_DUE", "Fornyelse forfalt");
+define("INV_REMINDER_INVOICE_OVERDUE", "Faktura forfalt");
+define("INV_REMINDER_USER_DEFINED", "Brukerdefinert");
 
 //Units
-define("INV_REMINDER_UNIT_DAYS", "Days");
-define("INV_REMINDER_UNIT_WEEKS", "Weeks");
-define("INV_REMINDER_UNIT_MONTHS", "Months");
+define("INV_REMINDER_UNIT_DAYS", "Dager");
+define("INV_REMINDER_UNIT_WEEKS", "Uker");
+define("INV_REMINDER_UNIT_MONTHS", "Måneder");
 
 //CRON script
-define("INV_REMINDER", "Reminder");
-define("INV_REMINDERS_SENT_INTRO", "%s Reminders have been processed by " . INV_BRANDING_NAME . ". Timestamp: %s.\n\n");
-define("INV_REMINDERS_SENT_SUBJECT", INV_BRANDING_NAME . " Scheduled Reminders");
-define("INV_REMINDERS_SENT_FAILURE", "FAILURE");
-define("INV_REMINDERS_SENT_NO_LICENSE", INV_BRANDING_NAME . " License Key Incorrect, or Not Found");
-define("INV_REMINDERS_SENT_TO", "sent to");
-define("INV_REMINDERS_SENT_FOR","for client");
-define("INV_REMINDERS_SENT_MESSAGE", "MESSAGE:");
-define("INV_REMINDER_SENT_ERROR", "WARNING! An error occurred whilst attempting to send this reminder. Reminder not sent.");
-define("INV_REMINDER_SENT_ERROR_NO_CLIENT", "WARNING! An error occurred whilst attempting to send this reminder. No client record was found. Reminder not sent.");
-define("INV_REMINDER_ORDER_NO", "Order No.");
+define("INV_REMINDER", "Påminnelse");
+define("INV_REMINDERS_SENT_INTRO", "%s Påminnelser er behandlet av " . INV_BRANDING_NAME . ". Tid: %s.\n\n");
+define("INV_REMINDERS_SENT_SUBJECT", INV_BRANDING_NAME . " Planlagt påminnelse");
+define("INV_REMINDERS_SENT_FAILURE", "FEIL");
+define("INV_REMINDERS_SENT_NO_LICENSE", INV_BRANDING_NAME . " Lisenskode feil eller ikke funnet");
+define("INV_REMINDERS_SENT_TO", "sendt til");
+define("INV_REMINDERS_SENT_FOR","for kunde");
+define("INV_REMINDERS_SENT_MESSAGE", "MELDING:");
+define("INV_REMINDER_SENT_ERROR", "ADVARSEL! Det oppstod en feil ved forsøk på å sende denne påminnelsen. Påminnelse ikke sendt.");
+define("INV_REMINDER_SENT_ERROR_NO_CLIENT", "ADVARSEL! Det oppstod en feil ved forsøk på å sende denne påminnelsen. Ingen kunde poster ble funnet. Påminnelse ikke sendt.");
+define("INV_REMINDER_ORDER_NO", "Ordrenr.");
 
 //Version 1.2.1
 //Note to translators: Text for INV_INSTR_REMINDER_EMAIL_TEXT updated to add description of {PAYLINK_HTTPS} (on line 51 of English language file)
