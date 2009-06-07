c13d11fec3c8384ceb8bf99a389adf38882c9c95
kncarlsen <kncarlsen@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-06-07 11:40:37 +0000

    Mangler en del oversettelser

---
+++ utvidelser/iJoomla Survey/norwegian.surveys.php
@@ -0,0 +1,349 @@
+<?php 
+
+// ensure this file is being included by a parent file
+
+defined( '_JEXEC' ) or die( 'Direkte adgang til dette stedet er ikke tillatt.' );
+######## FRONT END ###########################
+
+define("_READMORE","Les mere >>");
+define("_NEXT_BTN","Neste >>");
+define("_BACK_BTN","<< Tilbake");
+define("_SURVEY","Undersøkelse");
+define("_NAME1","Navn");
+define("_VIEW","Visning");
+define("_SUMMARY","Oppsummering");
+define("_RESPONSE","Respons");
+define("_PERCENT","Prosent");
+define("_RESPONDENTS","Respondenter");
+
+######## MENU LANGUAGE ITEMS #################
+
+define("LM_IJOOMLA_CP","iJoomla Undersøkelser Kontrollpanel");
+
+define("LM_MENU_SURVEY","iJoomla Undersøkelser");
+define("LM_MENU_ADMINISTRATION","Innstillinger");
+define("LM_MENU_CONFIGURATION","Generelt");
+define("LM_MENU_CSS","CSS");
+define("LM_MENU_LANGUAGE","Språk");
+define("LM_MENU_EMAIL","E-post");
+define("LM_MENU_MANAGERS","Adm. undersøkelser");
+define("LM_MENU_SURVEYS_MANAGER","Undersøkelser");
+define("LM_MENU_PAGES_MANAGER","Sider");
+define("LM_MENU_QUESIONS_MANAGER","Spørsmål");
+define("LM_MENU_SKIP_ACTIONS_MANAGER","Skip Actions");
+define("LM_MENU_DOCUMENTATION","Hjelp");
+define("LM_MENU_IJOOMLA_WEBSITE","iJoomla.com");
+define("LM_MENU_LATEST_VERSION","Siste versjon");
+define("LM_MENU_SUPPORT_HELP","Support");
+define("LM_MENU_FORUMS","Forum");
+define("LM_MENU_MANUAL","Manual");
+define("LM_MENU_OTHER_COMPONENTS","Andre komponenter");
+define("LM_MENU_TEMPLATES","Maler");
+
+define("LM_MENU_ABOUT","Om");
+
+define("_FRONTEND_TITLE","iJoomla undersøkelser");
+define("_SURVEY_NAME","Navn på undersøkelse");
+define("_NO_SURVEYS","Ingen undersøkelser funnet");
+define("_SURVEY_UNAVAILABLE","Undersøkelse ikke tilgjengelig");
+define("_NOT_CONNECTED","Du har ikke tillatelse til å fullføre denne undersøkelsen. Logg inn først");
+define("_REMEMBER_ME","Husk meg");
+define("_LOST_PASSWORD","Glemt passord?");
+define("_NO_ACCOUNT","Ingen konto ennå?");
+define("_REGISTER","Registrer");
+define("_NO_ANSWER","Ingen svar");
+define("_MM","MM");
+define("_DD","DD");
+define("_YYYY","ÅÅÅÅ");
+define("_LOGIN_REQUIRED","Denne undersøkelsen krever at brukeren er pålogget");
+define("_LINK_EMAIL","Lenke for en epostmelding");
+define("_LINK_WEB","Lenke for en nettside");
+define("_CLICK_TO_TAKE","Klikk her for å gjennomføre undersøkelsen");
+define("_CLICK_TO","Klikk for");
+define("_RESULT_TEXT","Resultattekst");
+define("_IP","IP");
+define("_ID","#id");
+define("_CLEAR","Clear");
+define("_VIEW_TEXT","Vis tekst");
+define("_ADD","Opprett >>");
+define("_REMOVE","<< Fjern");
+define("_EDIT_IMAGE","Rediger valgt bilde");
+define("_PLEASE_CREATE_PAGE","Opprett en side for denne undersøkelsen først");
+define("_PLEASE_CREATE_SURVEY","Opprett en undersøkelse");
+define("_PLEASE_SELECT_SURVEY","Velg navn på undersøkelse");
+define("_PLEASE_SELECT_PAGE","Velg sidenavn");
+define("_PLEASE_SELECT_QUESTION","Velg spørsmålsnavn");
+define("_PLEASE_SELECT_QUESTION_TYPE","velg spørsmålstype");
+define("_PLEASE_SELECT_SURVEY_AND_PAGE","Velg undersøkelsens navn og sidenavn først");
+define("_EDIT_SKIP","Edit Skip Action");
+define("_PAGE_WITHOUT_QUESTIONS","Denne siden har ingen spørsmål");
+define("_EQUAL","Lik");
+define("_DIFFERENT_THAN","Forskjellig fra");
+define("_AND","og");
+define("_OR","eller");
+define("_JUMP_TO","Hopp til");
+define("_YOU_CANT_ADD_SKIP_TO_LAST","You can't add skip logics to the last page of the survey");
+define("_PLEASE_SELECT_TO_COPY","Gjør et valg fra listen for å kopiere");
+define("_MUST_SUPPLY_VALID_URL","Du må angi en gyldig nettadresse");
+define("_MAXIMUM_VALUE_GREATER","Maksimumverdien må være støre enn minimumverdien");
+define("_YOU_MUST_ENTER","Du må registrere");
+define("_THE_MONTH","måned");
+define("_THE_DAY","dag");
+define("_THE_YEAR","år");
+define("_THE_NUMBERS_IN_INTERVAL","Tallene du registrerer må være i intervallet");
+define("_CHOICES_MUST_ADD_UP","Valgene må summeres til en total lik");
+define("_THEY_ADD_UP_TO","They currently add up to total of");
+define("_CHOICES_MUST_BE_NUMBERS","Valgene må være tall");
+define("_QUESTIONS_REQUIRED","Spørsmål som krever et svar er merket med en stjerne");
+define("_SELECT_ITEM_TO","Select an item to");
+define("_COPY","kopier");
+define("_DELETE","slett");
+define("_UNPUBLISH","avpubliser");
+define("_PLEASE_FILL_SURVEY_TITLE","Fyll inn tittel for undersøkelsen");
+define("_PLEASE_FILL_PAGE_TITLE","Fyll inn tittel for siden");
+define("_PLEASE_FILL_SKIP_NAME","Please fill in the Skip Action Name");
+define("_PLEASE_FILL_QUESTION_TITLE","Fyll inn tittel for spørsmålet");
+define("_PLEASE_FILL_COLUMN","Please fill the Column value if you choice Matrix Question");
+define("_FOLLOW_INSTRUCTIONS_FOR_SKIP","You must follow the instructions to complete the skip logic");
+define("_SELECT_ACTION_TARGET","Please select Action target Page");
+
+define("_YOU_CANT","Du kan ikke");
+define("_BECAUSE_PART_OF_SKIP","because it`s a part of a skip action.Please delete the skip action first");
+define("_THIS","denne");
+define("_PART_OF_SKIP_REQUIRED","is a part of a skip logic there for must be required");
+define("_MAKE","lag");
+define("_NOT","ikke");
+define("_QUESTION_CHANGED_TO_REQUIRED","The question you are trying to use is now being changed to required");
+define("_NOT_EMPTY_CHOICES","You are not allowed to enter non numbers or leave empty choices");
+define("_IF_COMPLETED","if you completed at least one");
+
+define("_COLLECT","Undersøkelseslenker");
+define("_CHANGE","Endre");
+define("_SETTING","Innstillinger");
+define("_COMPONENT","Komponent");
+define("_MANAGE","Administrer");
+define("_SURVEYS","Undersøkelser");
+define("_CLASSES","Klasser");
+define("_COMPONENTS","Komponenter");
+define("_MODULES","Moduler");
+define("_INSTALLED","Installert");
+define("_NOT_INSTALLED","Ikke installert");
+define("_DESCRIPTION","Beskrivelse");
+define("_QUESTION","Spørsmål");
+
+define("_EXPORT_DATA","Eksporter data");
+define("_STATUS","Status");
+define("_DOWNLOAD","Last ned");
+
+define("_PAGE","Side");
+define("_DATA","Data");
+define("_GENERAL","Generelt");
+define("_DATETIME","Dato/Tid"); 
+define("_ANSWER","Svar");
+define("_FORM","Skjema");
+define("_ELEMENTS","Elementer");
+
+define("_CHECKBOX","Check box");
+define("_RADIOBUTTON","Radio Button");
+define("_DROPDOWNMENU","Nedtrekksmeny");
+define("_TEXTAREA","Tekstområde");
+define("_INPUTBOX","Input Box");
+define("_BUTTON","Button");
+define("_TABLE","Tabell");
+define("_ROW","Rad");
+define("_HEADING","Overskrift");
+define("_COLUMN","Kolonne");
+define("_RESULTS","Resultater");
+define("_RESULT","Resultat");
+define("_BAR","Bar");
+define("_COLOR","Farge");
+define("_TOTAL","Totalt");
+define("_BACKGROUND","Bakgrunn");
+
+define("_VALUE","Verdi");
+define("_DISPLAY","Vis");
+define("_ACCESS","Adgang");
+define("_RESPONSES","Svar");
+define("_STATS","Stat.");
+define("_PAGES","Sider");
+define("_QUESTIONS","Spørsmål");
+define("_SHOW","Vis");
+define("_FINISHING","Finishing");
+define("_PUBLISHED","Publisert");
+define("_REORDER","Sorter på nytt");
+define("_ORDER","Sorter");
+define("_USERNAME","Brukernavn");
+define("_PASSWORD","Passord");
+define("_EMAIL","E-post");
+define("_FILTER","Filter");
+define("_ADDRESS","Adresse");
+define("_IN","i");
+define("_TO","til");
+define("_CHOOSE","velg");
+define("_DETAIL","Detaljer");
+define("_EDIT_SURVEY","Rediger undersøkelse");
+define("_NEW_SURVEY","Ny undersøkelse");
+
+define("_ONE_RESPONSE_P_RESPONDENT_VALUE","ORpR");
+
+define("_ONE_RESPONSE_P_RESPONDENT_TEXT","<b>One Response per Respondent</b> - After completing the survey, respondents will be prevented from 
+					entering additional responses. Respondents that return to a survey later will be able to edit their existing 
+					answers. Respondents that return to an incomplete survey will be taken to the point that they left off. ");
+
+define("_ONE_RESPONSE_P_RESPONDENT_FO_VALUE","ORpRFO");
+
+define("_ONE_RESPONSE_P_RESPONDENT_FO_TEXT","<b>One Response per Respondent (Forward Only)</b> - After completing the survey, respondents will be 
+					prevented from entering additional responses. In addition, respondents are prevented from backing up to 
+					edit their existing answers. Respondents that return to an incomplete survey will be taken to the point that 
+					they left off. ");
+
+define("_MULTIPLE_RESPONSES_P_RESPONDENT_VALUE","MRpR");
+
+define("_MULTIPLE_RESPONSES_P_RESPONDENT_TEXT","<b>Multiple Responses per Respondent</b> - After completing the survey, respondents will be allowed to 
+					enter an additional response. Respondents that return to an incomplete survey will be taken to the point that 
+					they left off.");
+
+define("_MULTIPLE_RESPONSES_P_RESPONDENT_SC_VALUE","MRpRSC");
+
+define("_MULTIPLE_RESPONSES_P_RESPONDENT_SC_TEXT","<b>Multiple Responses per Respondent (Shared Computer)</b> - After completing or exiting the survey, 
+					respondents will be allowed to enter additional responses. Once respondents leave the survey, their answers 
+					are considered finished and cannot be edited. Useful for computer labs and tradeshow kiosks.");
+
+define("_ONCE_A_DAY","En gang pr. dag");
+define("_ONCE_A_DAY_VALUE","1");
+define("_ONCE_A_WEEK","En gang i uken");
+define("_ONCE_A_WEEK_VALUE","7");
+define("_ONCE_A_MONTH","En gang pr.mnd");
+define("_ONCE_A_MONTH_VALUE","30");
+define("_ONCE_EVERY_3_MONTH","En gang hver tredje mnd.");
+define("_ONCE_EVERY_3_MONTH_VALUE","91");
+define("_ONCE_A_YEAR","En gang pr.år");
+define("_ONCE_A_YEAR_VALUE","365");
+define("_LIST","List");
+define("_CLOSE_PREVIEW","Lukk forhåndsvisning");
+define("_SURVEY_END","Undersøkelse avsluttes");
+define("_PREVIEW","Forhåndsvis");
+define("_DATE_TIME","Dato og tid for gjennomføringen");
+define("_SHOW_ON_RESULTS","Vis i resultatmodulen");
+define("_GO","Go");
+define("_YOU_MUST_ADD_SURVEY","Du må registrere minst en undersøkelse før du legger til en side");
+define("_SELECT_NUMBER_MENUS","Velg antall menyer pr. rad og rediger valgene for hver meny.");
+
+define("_HEADER","Overskrift");
+define("_TITLE","Tittel");
+define("_IMAGE_CONTROL","Bildekontroll");
+define("_SUB_FOLDER","Undermappe");
+define("_GALLERY_IMAGES","Albumbilder");
+define("_IMAGES","Bilder");
+define("_HANDLING","Handling");
+define("_PUBLISHING","Publiserer");
+define("_SHOW_ON_RESULT","Vis på resultat");
+define("_POP_UP","Pop up");
+define("_WIDTH","Bredde");
+define("_HEIGHT","Høyde");
+define("_STYLE","Stil");
+define("_CONTENT","Innhold");
+define("_INVITATION","Invitasjon");
+define("_YES","Ja");
+define("_NO","Nei");
+define("_ORDERING","Sortering");
+define("_FIRST","Først");
+define("_LAST","Sist");
+define("_LEVEL","Nivå");
+define("_OVERRIDE","Overstyr");
+define("_CREATED","Opprettet");
+define("_DATE","Dato");
+define("_START","Start");
+define("_END","Slutt");
+define("_FINISH","Avslutt");
+define("_TARGET","Mål");
+define("_SHOW_END_PAGE","Vis siste side");
+define("_URL_DEFAULT","http://");
+define("_REDIRECT_TO_URL","Rediriger til nettadresse");
+define("_REDIRECTION_URL","Redigeringsadresse");
+define("_REDIRECTION_MSG","Redirigeringsmelding");
+define("_ALL","Alle");
+define("_TYPE","Type");
+define("_FIELD_REQUIRED","Felt påkrevet");
+
+define("_QUESTION_SAVE","Spørsmål lagret");
+define("_QUESTION_APPLY","Endringer i spørsmålet ble lagret");
+define("_PAGE_SAVE","Side lagret");
+define("_PAGE_APPLY","Endringer på siden ble lagret");
+define("_SKIP_SAVE","Skip logic saved");
+define("_SKIP_APPLY","Changes made to the skip logic had been saved");
+
+define("_CHOICE_ONE_ANSWER_VERTICAL","Valg - ett svar (Vertikalt)");
+define("_CHOICE_ONE_ANSWER_HORIZONTAL","Valg - ett svar (horisontalt)");
+define("_CHOICE_ONE_ANSWER_MENU","Valg - ett svar (Meny)");
+define("_CHOICE_MULTIPLE_ANSWER_VERTICAL","Choice - Multiple Answers (Vertical)");
+define("_CHOICE_MULTIPLE_ANSWER_HORIZONTAL","Choice - Multiple Answers (Horizontal)");
+define("_MATRIX_ONE_ANSWER_PER_ROW","Matrix - One Answer per Row");
+define("_MATRIX_MULTIPLE_ANSWER_PER_ROW","Matrix - Multiple Answers per Row");
+define("_MATRIX_MULTIPLE_ANSWER_PER_ROW_MENUS","Matrix - Multiple Answers per Row (Menus)");
+define("_OPEN_ENDED_ONE_LINE_W_PROMPT","Open Ended - One Line w/Prompt");
+define("_OPEN_ENDED_ONE_MORE_LINE_W_PROMPT","Open Ended - One More Line w/Prompt");
+define("_OPEN_ENDED_ESSAY","Open Ended - Essay");
+define("_OPEN_ENDED_CONSTANT_SUM","Open Ended - Constant Sum");
+define("_OPEN_ENDED_DATE_AND_OR_TIME","Open Ended - Date and/or Time");
+
+//constants for email tab
+define("_NOTITY_ME_BY_EMAIL","Varsle meg via e-post når en undersøkelse er gjennomført");
+define("_SEND_EMAIL_NOTIFICATION_TO","Send e-postvarsling til");
+define("_FROM","Fra");
+define("_EMAIL_SUBJECT","E-postemne");
+define("_EMAIL_MESSAGE","E-postmelding");
+
+define("_EDIT","Rediger");
+define("_NEW","Ny");
+define("_PLEASE_SELECT","Vennligst velg ...");
+define("_MENUS","Menyer");
+define("_MENU","Meny");
+define("_CHOICES","Valg");
+define("_CHOICE","Valg");
+define("_REQUIRED","Påkrevet");
+define("_RANDOMIZE","Randomize");
+define("_ENTER_EARCH_CHOICE","Registrer hvert valg på egne linjer nedenfor.");
+define("_ENTER_EARCH_COLUMN","Registrer hvert kolonnenavn på egne linjer nedenfor.");
+define("_HAS_OTHER_ANSWER","Har andre svar");
+define("_OTHER_FIELD_S_TITLE","Andre felts tittel");
+define("_MANAGER","(Administrasjon)");
+define("_SKIP","Hopp over");
+define("_COMPARE","Sammenlign");
+define("_LOGIC","Logikk");
+define("_ACTION","Aksjon");
+define("_ACTIONS","Aksjoner");
+define("_ENTER_THE_TOTAL","Registrer totalen som svarene skal summeres til.<br />
+
+				For å tillate <b>enhver</b> sum, la feltet stå åpent.");
+define("_MIN_VALUE","Minimumverdi");
+define("_MAX_VALUE","Maksimumverdi");
+
+##############################################
+
+define("LM_POWERED_BY","Drevet av ");
+
+########### ABOUT FORM ##########################
+
+define("LM_ABOUT_SURVEY_INFO","Undersøkelsesinfo");
+
+define("LM_ABOUT","<h3>Om iJoomla Undersøkelser</h3>
+<br /><br />
+iJoomla Survey is a package of one component and two modules. It allows you to create a SURVEY layout for your Joomla site that either wouldn't be possible otherwise or would require a great deal of additional programming.
+<br /><br />
+Besøk vår side for mer informasjon:
+<br /><br />
+<a href='http://www.ijoomla.com'>http://www.ijoomla.com</a>
+<br /><br />
+All rights reserved. You may not distribute or sell this component, or use it or its modules, or any part of its program code, on more than a single domain.
+ ");
+
+# 11/04/2007 : _PLEASE_CREATE_SURVEY added
+
+# added in new version 34
+define("_SEND_EMAIL","Send e-post");
+define("_SEND_EMAIL_TO","Email to send to");
+define("_GLOBAL","Bruk globalt");
+
+define("_global","GLOBALT");
+?>
