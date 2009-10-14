e4d7df5e1ac90b776ea4fb2ac4c5f0c7073cbf86
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-10-14 06:45:19 +0000

    Flytter arkiv Opprettet j16

--- kjerne/tags/nb-NO_Joomla_1.5_Stable_Khepri_KOMP/includes/HTML_toolbar.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Legacy Mode compatibility
- * @version		$Id: HTML_toolbar.php 7692 2007-06-08 20:41:29Z tcp $
- * @package		Joomla.Legacy
- */
-require_once( dirname(__FILE__).'/../plugins/system/legacy/toolbar.php' );
--- kjerne/tags/nb-NO_Joomla_1.5_Stable_Khepri_KOMP/includes/vcard.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Legacy Mode compatibility
- * @version		$Id: vcard.class.php 7692 2007-06-08 20:41:29Z tcp $
- * @package		Joomla.Legacy
- */
-require_once( dirname(__FILE__)  .'/../libraries/bitfolge/vcard.php' );
