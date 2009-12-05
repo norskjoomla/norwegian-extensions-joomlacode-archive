2bc00fc2f3850ac0cf417bd70334dda451cef8fe
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-12-05 05:25:10 +0000

    Kopiert og endret navn fra v1

--- utvidelser/nBill/v2/nb-NO/template.in.nb-NO.php
+++ utvidelser/nBill/v2/nb-NO/template.in.nb-NO.php
@@ -1,14 +1,13 @@
 <?php
 /**
-* Language file for the default invoice template
-* @version 1
+* @version 1.1.x
 * @package nBill
-* @copyright (C) 2006-2009 Netshine Software Limited
-* @license http://www.nbill.co.uk/eula.html
+* @copyright (C) 2006-2007 Netshine Software Limited
+* @copyright Translation 2009 Joomla! i Norge
+* @license Commercial
 * 
-* @access private
 * PLEASE NOTE: This is NOT free software. You must purchase a license in order to use this component.
-* For more information, see www.nbill.co.uk and the license agreement at www.nbill.co.uk/eula.html
+* For more information, see www.nbill.co.uk
 *
 * All Rights Reserved. You may make amendments to any unencrypted files for your own use only or
 * for the use of your customers if you are a website developer. HOWEVER, you are not permitted to
@@ -19,24 +18,45 @@
 * software is entirely at your own risk.
 */
 
-//Ensure this file has been reached through a valid entry point (not always necessary eg. for class files, but included on every file to be safe!)
-(defined('_VALID_MOS') || defined('_JEXEC') || defined('NBILL_VALID_NBF')) or die('Access Denied.');
+/** ensure this file is being included by a parent file */
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
 //Invoicing Template
-define("NBILL_PRT_INVOICE_TITLE", "INVOICE");
-define("NBILL_PRT_INVOICE_NO", "Invoice No:");
-define("NBILL_PRT_AMOUNT_DUE", "Invoice Total:");
-define("NBILL_PAYMENT_RECEIVED", "The following payment(s) have been received (thank you):");
-define("NBILL_FULL_PAYMENT_RECEIVED", "Paid in Full. Thank you.");
-define("NBILL_REMITTANCE_ADVICE", "Remittance Advice");
-define("NBILL_REMITTANCE_INTRO", "If sending payment in the mail, please enclose this remittance advice to enable us to identify what your payment is for. Thank you.");
-define("NBILL_RECEIVED_FROM", "Received From");
-define("NBILL_PAYMENT_DATE", "Payment Date");
-define("NBILL_PAYMENT_METHOD", "Method");
-define("NBILL_PAYMENT_AMOUNT" , "Payment Amount");
-define("NBILL_PAYMENT_REFERENCE", "Our Reference");
-define("NBILL_TOTAL_PAID", "Total Amount Paid");
-define("NBILL_TOTAL_DUE", "Amount Outstanding:");
-define("NBILL_REFERENCE_UNKNOWN", "Not Yet Assigned");
-define("NBILL_IF_NO_SCHEDULE", "If you do not already have a payment schedule set up for this, please");
-define("NBILL_PAY_THIS_INVOICE", "click here to pay this invoice");
+define("INV_PRT_INVOICE_TITLE", "FAKTURA");
+define("INV_PRT_INVOICE_NO", "Fakturanr.:");
+define("INV_PRT_REFERENCE", "Referanse:");
+define("INV_PRT_DATE", "Fakturadato:");
+define("INV_PRT_CLIENT_TAX_REF", "Kundens MVA.nr.:");
+define("INV_PRT_DESC", "Beskrivelse");
+define("INV_PRT_UNIT_PRICE", "Stykkpris");
+define("INV_PRT_QUANTITY", "Antall");
+define("INV_PRT_DISCOUNT", "Rabatt");
+define("INV_PRT_NET_PRICE", "Netto");
+define("INV_PRT_VAT", "MVA");
+define("INV_PRT_CARRIAGE", "Frakt");
+define("INV_PRT_CARRIAGE_VAT", "%s på frakt");
+define("INV_PRT_TOTAL", "Totalt");
+define("INV_PRT_NET_AMOUNT", "Nettobeløp:");
+define("INV_PRT_AMOUNT_DUE", "Totalsum:");
+define("INV_PAYMENT_RECEIVED", "Vi takker for mottatt betaling");
+define("INV_PRT_INVOICE_TITLE_CR", "KREDITNOTA");
+define("INV_PRT_INVOICE_NO_CR", "Kreditnotanr.:");
+define("INV_PRT_AMOUNT_DUE_CR", "Refundert beløp:");
+define("INV_PAYMENT_RECEIVED_CR", "Utbetaling av denne kreditnotaen er behandlet.");
+define("INV_REMITTANCE_ADVICE", "Betalingsinformasjon");
+define("INV_REMITTANCE_INTRO", "Vennligst legg ved denne betalingsinformasjonen om du sender betaling i posten, slik at vi kan se hva betalingen din gjelder. På forhånd takk!");
+define("INV_RECEIVED_FROM", "Mottatt fra");
+define("INV_PAYMENT_DATE", "Betalingsdato");
+define("INV_PAYMENT_METHOD", "Betalingsmåte");
+define("INV_PAYMENT_REFERENCE", "Vår referanse");
+define("INV_CHEQUE", "Sjekk");
+define("INV_DIRECT_DEBIT", "Direktetrekk");
+define("INV_STANDING_ORDER", "Ventende ordre");
+define("INV_BANK_TRANSFER", "Bankoverføring");
+define("INV_CREDIT_CARD", "Kreditt-/Debetkort");
+define("INV_ONLINE_AGENCY", "Online");
+define("INV_CASH", "Kontant");
+define("INV_OTHER", "Annet");
+define("INV_IF_NO_SCHEDULE", "Om du ønsker å betale denne på en annen måte kan du");
+define("INV_PAY_THIS_INVOICE", "klikke her for å betale på nett");
+?>
