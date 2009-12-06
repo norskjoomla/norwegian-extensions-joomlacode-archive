dc08ab68ddb0d54f92d6011cd733e9d455623733
knuthel <knuthel@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-12-06 10:55:46 +0000

    Lagt til ny versjon og oppdatert noen språkfiler og laget en ny språkpakke for nedlasting

---
+++ utvidelser/JCE/JCE_157_154/tiny_mce/plugins/searchreplace/langs/nb_dlg.js
@@ -0,0 +1,19 @@
+tinyMCE.addI18n('nb.searchreplace_dlg',{
+searchnext_desc:"Finn igjen",
+notfound:"Søket er ferdig, søketeksten ble ikke funnet.",
+search_title:"Søk",
+replace_title:"Søk/erstatt",
+allreplaced:"Alle steder der søketeksten ble funnet ble erstattet.",
+findwhat:"Finn hva?",
+replacewith:"Erstatt med",
+direction:"Retning",
+up:"Oppover",
+down:"Nedover",
+mcase:"Match store/små bokstaver",
+findnext:"Finn neste",
+replace:"Erstatt",
+replaceall:"Erstatt alle"
+});
+tinyMCE.addI18n('nb.searchreplace',{
+search_desc : "Søk"
+});
