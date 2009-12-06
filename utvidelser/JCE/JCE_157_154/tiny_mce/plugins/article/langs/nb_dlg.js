5f7a72e11b4b06827afc318eee81a7f2a78e0885
knuthel <knuthel@6aa871c5-9e2b-0410-bde1-d651c692654a>
2009-12-06 10:55:46 +0000

    Lagt til ny versjon og oppdatert noen språkfiler og laget en ny språkpakke for nedlasting

---
+++ utvidelser/JCE/JCE_157_154/tiny_mce/plugins/article/langs/nb_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('nb.pagebreak_dlg',{
+desc:"Sett inn/endre sidebrytning",
+title:"Sidetittel",
+alias:"Alias for innholdsfortegnelsen"
+});
---
+++ utvidelser/JCE/JCE_157_154/tiny_mce/plugins/imgmanager/langs/nb_dlg.js
@@ -0,0 +1,6 @@
+tinyMCE.addI18n('nb.imgmanager_dlg',{
+missing_alt: "Er du sikker på at du vil fortsette uten bildebeskrivelse? Uten beskrivelse vil bildet være utilgjengelig for brukere med synshemmelser eller andre handikap, og de som bruker tekstbaserte nettlesere, vanlig på mobiltelefoner mm.",
+delta_width: 0,
+delat_height: 0,
+no_src: 'En URL er obligatorisk. Velg et bilde eller skriv inn en URL'
+});
