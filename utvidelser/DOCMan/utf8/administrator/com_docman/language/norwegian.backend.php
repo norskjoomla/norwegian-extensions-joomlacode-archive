3188e64e8cf09515566cee9afba2baf22b7c07de
kncarlsen <kncarlsen@users.noreply.github.com>
2009-12-16 13:37:28 +0000

    Kjørt diff. Ingen endringer i språkfilen fra 1.4

--- utvidelser/DOCMan/utf8/administrator/com_docman/language/norwegian.backend.php
+++ utvidelser/DOCMan/utf8/administrator/com_docman/language/norwegian.backend.php
@@ -1,26 +1,27 @@
 <?php
 /**
- * DOCman 1.4.x - Joomla! Document Manager
- * @version $Id$
- * @package DOCman_1.4
- * @copyright (C) 2003-2007 The DOCman Development Team
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.joomlatools.org/ Official web site
- **/
-defined('_VALID_MOS') or die('Restricted access');
+ * @version		$Id$
+ * @category	DOCman
+ * @package		DOCman15
+ * @copyright	Copyright (C) 2003 - 2009 Johan Janssens and Mathias Verraes. All rights reserved.
+ * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link     	http://www.joomladocman.org
+ */
+defined('_JEXEC') or die('Restricted access');
 
 /**
  * TRANSLATORS:
- * PLEASE CHANGE THE INFO BELOW
- *
- * Default Norwegian language file
- *
- * Creator:  Rune Rasmussen
- * Website:  http://www.syntaxerror.no/
- * E-mail:   nopsam@syntaxerror.no
- * Revision: 1.4
- * Date:     September 2007
- **/
+ * PLEASE ADD THE INFO BELOW
+ */
+ 
+/**
+ * Language: Norwegian
+ * Creator: Rune Rasmussen
+ * Website: http://www.syntaxerror.no/
+ * E-mail: nospam@syntaxerror.no
+ * Revision: 1.5
+ * Date: Desember 2009
+ */
 
 // -- Toolbar
 define('_DML_TOOLBAR_SAVE', "Lagre");
