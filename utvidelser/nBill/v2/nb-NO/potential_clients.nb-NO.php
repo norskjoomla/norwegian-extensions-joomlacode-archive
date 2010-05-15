958b530a51a529a12c57ed04d97746ff6b705240
roar <roar@6aa871c5-9e2b-0410-bde1-d651c692654a>
2010-05-15 22:41:00 +0000

    Klar for korrektur

--- utvidelser/nBill/v2/nb-NO/potential_clients.nb-NO.php
+++ utvidelser/nBill/v2/nb-NO/potential_clients.nb-NO.php
@@ -22,7 +22,7 @@
 //Ensure this file has been reached through a valid entry point (not always necessary eg. for class files, but included on every file to be safe!)
 (defined('_VALID_MOS') || defined('_JEXEC') || defined('NBILL_VALID_NBF')) or die('Access Denied.');
 
-define("NBILL_CLIENTS_TITLE_POT", "Potential Clients");
-define("NBILL_CLIENT_INTRO_POT", "Potential clients are those who have requested a quote or shown an interest in your services, but have never actually ordered anything. Potential client records are typically created automatically when someone fills in a quote request form in your website front-end, but you can create them manually as an administrator too. If a quote is accepted, the potential client is converted to an actual client automatically, but again, as an administrator, you can 'promote' a potential client to an actual client yourself manually (whether or not they have any orders).");
-define("NBILL_EDIT_CLIENT_POT", "Edit Potential Client");
-define("NBILL_NEW_CLIENT_POT", "New Potential Client");
+define("NBILL_CLIENTS_TITLE_POT", "Potensielle kunder");
+define("NBILL_CLIENT_INTRO_POT", "Potensielle kunder er de som har bedt om et tilbud eller vist interesse for tjenestene, men har aldri bestilt noe. Potensielle kunder opprettes automatisk når noen fyller i en tilbudsforespørsel på nettsteds brukerside, og du kan også opprette dem manuelt som administrator. Hvis et tilbud blir akseptert, blir den potensielle kunden automatisk konvertert til en faktisk kunde, men igjen, som administrator, kan du ’flytte’ en potensiell kunde til en faktisk kunde manuelt (hvorvidt om de har bestilt noen eller ikke).");
+define("NBILL_EDIT_CLIENT_POT", "Rediger potensielle kunder");
+define("NBILL_NEW_CLIENT_POT", "Nye potensielle kunder");
--- utvidelser/nBill/v2/nb-NO/products.nb-NO.php
+++ utvidelser/nBill/v2/nb-NO/products.nb-NO.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Language file for nBill
-* @version 1
+* Language file for Products
+* @version 2
 * @package nBill
 * @copyright (C) 2006-2009 Netshine Software Limited
 * @license http://www.nbill.co.uk/eula.html
@@ -37,45 +37,45 @@ define("NBILL_PRODUCT_NAME_REQUIRED", "Angi et navn for produktet eller tjeneste
 define("NBILL_PRODUCT_DETAILS", "Produktdetaljer");
 define("NBILL_NET_PRICE_ONE_OFF", "Netto pris en gang");
 define("NBILL_NET_PRICE_WEEKLY", "Netto pris ukentlig");
-define("NBILL_NET_PRICE_MONTHLY", "Netto pris mÂnedlig");
+define("NBILL_NET_PRICE_MONTHLY", "Netto pris m√•nedlig");
 define("NBILL_NET_PRICE_QUARTERLY", "Netto pris kvartalsvis");
-define("NBILL_NET_PRICE_ANNUALLY", "Netto pris Ârlig");
-define("NBILL_NET_PRICE_BIANNUALLY", "Netto pris 2-Ârig");
-define("NBILL_NET_PRICE_FIVE_YEARLY", "Netto pris 5-Ârig");
-define("NBILL_NET_PRICE_TEN_YEARLY", "Netto pris 10-Ârig");
+define("NBILL_NET_PRICE_ANNUALLY", "Netto pris √•rlig");
+define("NBILL_NET_PRICE_BIANNUALLY", "Netto pris 2-√•rig");
+define("NBILL_NET_PRICE_FIVE_YEARLY", "Netto pris 5-√•rig");
+define("NBILL_NET_PRICE_TEN_YEARLY", "Netto pris 10-√•rig");
 define("NBILL_IS_TAXABLE", "Avgiftspliktig?");
-define("NBILL_REQUIRES_CARRIAGE", "Legg til forsendelsesavgift?");
-define("NBILL_CARRIAGE_SERVICES", "Forsendelse tilgjengelig");
-define("NBILL_CARRIAGE_UNITS", "Forsendelsesenhet");
-define("NBILL_AUTO_FULFIL", "Fullf¯re ordre automatisk?");
+define("NBILL_REQUIRES_SHIPPING", "Legg til forsendelsesavgift?");
+define("NBILL_SHIPPING_SERVICES", "Forsendelse tilgjengelig");
+define("NBILL_SHIPPING_UNITS", "Forsendelsesenhet");
+define("NBILL_AUTO_FULFIL", "Fullf√∏re ordre automatisk?");
 define("NBILL_IS_DOWNLOADABLE", "Er nedlastbar?");
 define("NBILL_DOWNLOAD_LOCATION", "Nedlastingssted");
 define("NBILL_NO_OF_DAYS_AVAILABLE", "Antall dager tilgjengelig");
-define("NBILL_DOWNLOAD_LINK_TEXT", "Tekst pÂ nedlastningslink");
-define("NBILL_INSTR_PRODUCT_SKU", "Et \"varenummer\" er en kode som du bruker til Â identifisere dette produktet. Du kan bruke hvilken som helst kode.");
+define("NBILL_DOWNLOAD_LINK_TEXT", "Tekst p√• nedlastningslink");
+define("NBILL_INSTR_PRODUCT_SKU", "Et 'varenummer' er en kode som du bruker til √• identifisere dette produktet. Du kan bruke hvilken som helst kode.");
 define("NBILL_INSTR_PRODUCT_NAME", "");
 define("NBILL_INSTR_PRODUCT_CATEGORY", "");
 define("NBILL_INSTR_PATH_TO_IMAGE", "This is not currently used, but might be implemented at a later date for use in conjunction with a shopping cart.");
 define("NBILL_INSTR_IS_FREEBIE", "Hvis dette produktet leveres gratis, angi det her, ellers fyller du inn en eller flere av prisboksene nedenfor.");
-define("NBILL_INSTR_NET_PRICE_ONE_OFF", "Hvis dette produktet skal kj¯pes direkte (snarere enn Â betalt for med jevnelige mellomrom), angi nettopris her.");
+define("NBILL_INSTR_NET_PRICE_ONE_OFF", "Hvis dette produktet skal kj√∏pes direkte (snarere enn √• betalt for med jevnelige mellomrom), angi nettopris her.");
 define("NBILL_INSTR_NET_PRICE_WEEKLY", "Hvis produktet skal betales ukentlig skriver du inn nettopris pr. uke.");
-define("NBILL_INSTR_NET_PRICE_MONTHLY", "Hvis produktet skal betales mÂnedlig skriver inn nettopris pr. mÂned.");
+define("NBILL_INSTR_NET_PRICE_MONTHLY", "Hvis produktet skal betales m√•nedlig skriver inn nettopris pr. m√•ned.");
 define("NBILL_INSTR_NET_PRICE_QUARTERLY", "Hvis produktet skal betales kvartalsvis, skriver inn netto pris per kvartal.");
-define("NBILL_INSTR_NET_PRICE_ANNUALLY", "Hvis produktet skal betales Ârlig skriver du inn nettopris pr. Âr.");
-define("NBILL_INSTR_NET_PRICE_BIANNUALLY", "Hvis produktet skal betales hvert andre Âr skriver du inn nettopris pr. 2 Âr.");
-define("NBILL_INSTR_NET_PRICE_FIVE_YEARLY", "Hvis produktet skal betales hvert femte Âr skriver du inn nettopris pr. 5 Âr.");
-define("NBILL_INSTR_NET_PRICE_TEN_YEARLY", "Hvis produktet skal betales hvert tiende Âr skriver du inn nettopris pr. 10 Âr.");
+define("NBILL_INSTR_NET_PRICE_ANNUALLY", "Hvis produktet skal betales √•rlig skriver du inn nettopris pr. √•r.");
+define("NBILL_INSTR_NET_PRICE_BIANNUALLY", "Hvis produktet skal betales hvert andre √•r skriver du inn nettopris pr. 2 √•r.");
+define("NBILL_INSTR_NET_PRICE_FIVE_YEARLY", "Hvis produktet skal betales hvert femte √•r skriver du inn nettopris pr. 5 √•r.");
+define("NBILL_INSTR_NET_PRICE_TEN_YEARLY", "Hvis produktet skal betales hvert tiende √•r skriver du inn nettopris pr. 10 √•r.");
 define("NBILL_INSTR_IS_TAXABLE", "Velg 'Nei' hvis du alltid vil utelate skatt for dette produktet, ellers velger du 'ja'.");
-define("NBILL_INSTR_REQUIRES_CARRIAGE", "Skal forsendelsesavgift legges til prisen for dette produktet?");
-define("NBILL_INSTR_CARRIAGE_SERVICES_AVAILABLE", "Velg leveringstjenester som kan brukes med dette elementet hvis aktuelt.");
-define("NBILL_INSTR_CARRIAGE_UNITS", "Den angitte forsendelsesavgift vil bli multiplisert med verdien for den valgte tjenesten for Â beregne de totale transportkostnadene (for tyngre varer som koster mer Â sende skriv inn en verdi som er st¯rre enn 1). Det er tillatt Â bruke desimaler.");
+define("NBILL_INSTR_REQUIRES_SHIPPING", "Skal forsendelsesavgift legges til prisen for dette produktet?");
+define("NBILL_INSTR_SHIPPING_SERVICES_AVAILABLE", "Velg leveringstjenester som kan brukes med dette elementet hvis aktuelt.");
+define("NBILL_INSTR_SHIPPING_UNITS", "Den angitte forsendelsesavgift vil bli multiplisert med verdien for den valgte tjenesten for √• beregne de totale transportkostnadene (for tyngre varer som koster mer √• sende skriv inn en verdi som er st√∏rre enn 1). Det er tillatt √• bruke desimaler.");
 define("NBILL_INSTR_AUTO_FULFIL", "Indikere om alle bestillinger for denne artikkelen automatisk skal sattes til status 'Komplett' (f. eks for umiddelbare nedlastinger).");
-define("NBILL_INSTR_IS_DOWNLOADABLE", "Hvorvidt kunden skal kunne laste ned dette produktet fra omrÂde 'Min konto'.");
-define("NBILL_INSTR_DOWNLOAD_LOCATION", "Absolutt sti til nedlastingsfilen (f.eks. http://nettsted/hjem/brukernavn/filer/mittdokument.pdf). Denne plasseringen b¯r ikke vÊre i den offentlige delen av nettstedet ditt. (Ikke bruk en mappe under din public_html eller htdocs mappe, ellers kan alle laste det ned uten Â betale!)");
+define("NBILL_INSTR_IS_DOWNLOADABLE", "Hvorvidt kunden skal kunne laste ned dette produktet fra omr√•de 'Min konto'.");
+define("NBILL_INSTR_DOWNLOAD_LOCATION", "Absolutt sti til nedlastingsfilen (f.eks. http://nettsted/hjem/brukernavn/filer/mittdokument.pdf). Denne plasseringen b√∏r ikke v√¶re i den offentlige delen av nettstedet ditt. (Ikke bruk en mappe under din public_html eller htdocs mappe, ellers kan alle laste det ned uten √• betale!)");
 define("NBILL_INSTR_DOWNLOAD_LINK_TEXT", "Tekst som skal vises som nedlastningslenke.");
-define("NBILL_INSTR_NO_OF_DAYS_AVAILABLE", "Antall dager filen(e) skal vÊre tilgjengelig for nedlastning pÂ kundens omrÂde íMin kontoí");
-define("NBILL_PRODUCT_PRICE_INTRO", "Angi prisen(e) pÂ produktet for hver valuta som du ¯nsker Â selge dette produktet i.");
-define("NBILL_DOWNLOAD_INFO_REQUIRED", "Du har angitt at dette er et nedlastbart produkt, men har ikke oppgitt nedlastingslenke og/eller teksten for lenken. Kontroller at begge disse verdiene er angitt dersom du ¯nsker at dette produktet skal vÊre nedlastbart.");
+define("NBILL_INSTR_NO_OF_DAYS_AVAILABLE", "Antall dager filen(e) skal v√¶re tilgjengelig for nedlastning p√• kundens omr√•de ‚ÄôMin konto‚Äô");
+define("NBILL_PRODUCT_PRICE_INTRO", "Angi prisen(e) p√• produktet for hver valuta som du √∏nsker √• selge dette produktet i.");
+define("NBILL_DOWNLOAD_INFO_REQUIRED", "Du har angitt at dette er et nedlastbart produkt, men har ikke oppgitt nedlastingslenke og/eller teksten for lenken. Kontroller at begge disse verdiene er angitt dersom du √∏nsker at dette produktet skal v√¶re nedlastbart.");
 define("NBILL_DOWNLOAD_LOCATION_2", "Nedlastingssted 2");
 define("NBILL_INSTR_DOWNLOAD_LOCATION_2", "Absolutt sti til fil nummer 2 (du kan ha opptil 3 filer).");
 define("NBILL_DOWNLOAD_LINK_TEXT_2", "Tekst nedlastningslenke (fil 2).");
@@ -86,40 +86,40 @@ define("NBILL_DOWNLOAD_LINK_TEXT_3", "Tekst nedlastningslenke (fil 3).");
 define("NBILL_INSTR_DOWNLOAD_LINK_TEXT_3", "Tekst som skal vises som nedlastningslenke (fil 3).");
 define("NBILL_IS_USER_SUB", "Brukerabonnement?");
 define("NBILL_INSTR_IS_USER_SUB", "Vise om dette produktet representerer et brukerabonnement.");
-define("NBILL_SUB_ACCESS_LEVEL", "Brukergruppe tilgangsnivÂ");
-define("NBILL_INSTR_SUB_ACCESS_LEVEL", "Brukergruppe tilgangsnivÂ for brukere som kj¯per dette abonnementet.");
-define("NBILL_EXPIRY_LEVEL", "Brukergruppe tilgangsnivÂ utl¯per");
-define("NBILL_INSTR_EXPIRY_LEVEL", "Hva skal du gj¯re med brukerens privilegier nÂr abonnement utl¯per (OBS: Om kj¯pet er en engangsbetaling vil abonnementet aldri utl¯pe).");
+define("NBILL_SUB_USER_GROUP", "Brukergruppe tilgangsniv√•");
+define("NBILL_INSTR_SUB_USER_GROUP", "Brukergruppe tilgangsniv√• for brukere som kj√∏per dette abonnementet.");
+define("NBILL_EXPIRY_LEVEL", "Brukergruppe tilgangsniv√• utl√∏per");
+define("NBILL_INSTR_EXPIRY_LEVEL", "Hva skal du gj√∏re med brukerens privilegier n√•r abonnement utl√∏per (OBS: Om kj√∏pet er en engangsbetaling vil abonnementet aldri utl√∏pe).");
 define("NBILL_EXPIRY_REDIRECT", "Omadressere etter forfall");
-define("NBILL_INSTR_EXPIRY_REDIRECT", "Nettadresse Â omadressere til etter at et abonnement er utl¯pt (dvs. etter at brukeren har blitt nedgradert, blokkert eller slettet). 'Ingen' betyr at ingen omdirigeringen vil skje, 'Standard' betyr at " . NBILL_BRANDING_NAME . " vil lage en passende melding, eller du kan velge din egen nettadresse Â viderekoble til.");
+define("NBILL_INSTR_EXPIRY_REDIRECT", "Nettadresse √• omadressere til etter at et abonnement er utl√∏pt (dvs. etter at brukeren har blitt nedgradert, blokkert eller slettet). 'Ingen' betyr at ingen omdirigeringen vil skje, 'Standard' betyr at " . NBILL_BRANDING_NAME . " vil lage en passende melding, eller du kan velge din egen nettadresse √• viderekoble til.");
 define("NBILL_REDIRECT_NONE", "Ingen");
 define("NBILL_REDIRECT_DEFAULT", "Standard");
-define("NBILL_REDIRECT_URL", "F¯lgende nettadresse");
+define("NBILL_REDIRECT_URL", "F√∏lgende nettadresse");
 define("NBILL_EXPIRY_DELETE", "[Slett bruker]");
 define("NBILL_EXPIRY_BLOCK", "[Sperr bruker]");
 define("NBILL_EXPIRY_DOWNGRADE", "Sett til %s");
-define("NBILL_ENSURE_MAMBOT_PUBLISHED", "MERK: Siden dette er et abonnement, sjekk at du har installert og publisert tillegget som hÂndterer utl¯psabonnement (mambot) - tilgjengelig fra " . NBILL_BRANDING_WEBSITE);
+define("NBILL_ENSURE_MAMBOT_PUBLISHED", "MERK: Siden dette er et abonnement, sjekk at du har installert og publisert tillegget som h√•ndterer utl√∏psabonnement (mambot) - tilgjengelig fra " . NBILL_BRANDING_WEBSITE);
 define("NBILL_ALLOW_GLOBAL_DISCOUNTS", "Tillat globale rabatter?");
-define("NBILL_INSTR_ALLOW_GLOBAL_DISCOUNTS", "Indikere om global rabatt ogsÂ gjelder for ordre pÂ dette produktet.");
+define("NBILL_INSTR_ALLOW_GLOBAL_DISCOUNTS", "Indikere om global rabatt ogs√• gjelder for ordre p√• dette produktet.");
 define("NBILL_PRODUCT_DISCOUNT_TITLE", "Produktspesifikke rabatter");
-define("NBILL_PRODUCT_DISCOUNT_INTRO", "Du kan eventuelt angi at visse rabatter gjelder for disse produktene. Hvis mer enn Èn rabatt er aktuelt, bruk prioritetsverdien til Â indikere hvilken som skal brukes f¯rst (Rabatt med prioritet pÂ 0 vil bli brukt f¯r en rabatt med prioritet pÂ 1). Prioriteringer tildelt her overstyrer eventuelle prioriteringer satt andre steder. For Â kontrollere egenskapene pÂ rabatten (f.eks. om bel¯pet er eksklusivt, etc.), rediger rabatt via rabattsiden. Hvis du bare vil at rabatt skal gjelde nÂr et bestemt antall av produktet er kj¯pt kan du angi dette i minimumsfeltet. Du kan ogsÂ eventuelt multiplisere mengden rabatt med mengde bestilt (rabatt bare pÂ spesifikke fastprisprodukter). Hvis du bruker kvantum som multiplikator kan du ogsÂ spesifisere en korreksjon som skal trekkes fra multiplikatormetoden (f.eks. hvis du vil at rabatten bare skal gjelde for den 2. og deretter etterf¯lgende antall, men ikke for den f¯rste settes utlignet til 1).");
+define("NBILL_PRODUCT_DISCOUNT_INTRO", "Du kan eventuelt angi at visse rabatter gjelder for disse produktene. Hvis mer enn √©n rabatt er aktuelt, bruk prioritetsverdien til √• indikere hvilken som skal brukes f√∏rst (Rabatt med prioritet p√• 0 vil bli brukt f√∏r en rabatt med prioritet p√• 1). Prioriteringer tildelt her overstyrer eventuelle prioriteringer satt andre steder. For √• kontrollere egenskapene p√• rabatten (f.eks. om bel√∏pet er eksklusivt, etc.), rediger rabatt via rabattsiden. Hvis du bare vil at rabatt skal gjelde n√•r et bestemt antall av produktet er kj√∏pt kan du angi dette i minimumsfeltet. Du kan ogs√• eventuelt multiplisere mengden rabatt med mengde bestilt (rabatt bare p√• spesifikke fastprisprodukter). Hvis du bruker kvantum som multiplikator kan du ogs√• spesifisere en korreksjon som skal trekkes fra multiplikatormetoden (f.eks. hvis du vil at rabatten bare skal gjelde for den 2. og deretter etterf√∏lgende antall, men ikke for den f√∏rste settes utlignet til 1).");
 define("NBILL_PRODUCT_DISCOUNT", "Rabatt");
 define("NBILL_PRODUCT_DISCOUNT_PRIORITY", "Prioritet");
 define("NBILL_PRODUCT_DISCOUNT_QTY", "Minimum antall");
 define("NBILL_ADD_PRODUCT_DISCOUNT", "Ny produktrabatt");
 define("NBILL_DELETE_DISCOUNT", "Slett produktrabatt");
-define("NBILL_PLEASE_SELECT_DISCOUNT", "Velg en rabatt Â legge til.");
+define("NBILL_PLEASE_SELECT_DISCOUNT", "Velg en rabatt √• legge til.");
 define("NBILL_PRODUCT_DISCOUNT_DUPLICATION", "Denne rabatten finnes allerede.");
 define("NBILL_INSTR_PRODUCT_DESCRIPTION", "This is not currently used, but might be implemented at a later date for use in conjunction with a shopping cart.");
 define("NBILL_NET_PRICE_SETUP_FEE", "Registreringsavgift");
-define("NBILL_INSTR_NET_PRICE_SETUP_FEE", "Om det er en engangs registreringsavgift i tillegg til prisen pÂ produktene angir du det her. Denne avgiften vil kun bli belastet pÂ den f¯rste betalingen og kommer som tillegg til den vanlige prisen pÂ produktene. Du kan angi en negativ verdi dersom du ¯nsker Â tilby et gratis eller en rabattert pr¯veperiode (dersom dette er st¯ttet av din betalingskanal).");
-define("NBILL_ERR_PRODUCT_IN_USE", "Beklager, en eller flere produkter du har valgt Â slette er i bruk pÂ f¯lgende ordre: %s. S¯rg for at produktene ikke er i bruk f¯r du sletter de.");
+define("NBILL_INSTR_NET_PRICE_SETUP_FEE", "Om det er en engangs registreringsavgift i tillegg til prisen p√• produktene angir du det her. Denne avgiften vil kun bli belastet p√• den f√∏rste betalingen og kommer som tillegg til den vanlige prisen p√• produktene. Du kan angi en negativ verdi dersom du √∏nsker √• tilby et gratis eller en rabattert pr√∏veperiode (dersom dette er st√∏ttet av din betalingskanal).");
+define("NBILL_ERR_PRODUCT_IN_USE", "Beklager, en eller flere produkter du har valgt √• slette er i bruk p√• f√∏lgende ordre: %s. S√∏rg for at produktene ikke er i bruk f√∏r du sletter de.");
 define("NBILL_EMAIL_DOWNLOADS", "E-post nedlastning?");
-define("NBILL_INSTR_EMAIL_DOWNLOADS", "Hvorvidt filene definert over skal sendes eller ikke nÂr kunden bestiller produktet (dette vil skje nÂr bestillingen opprettes - sÂ hvis du har \"Vent inntil betalt\" satt til \"Ja\" pÂ bestillingsskjemaet, vil de ikke fÂ filene f¯r de har betalt for de).");
+define("NBILL_INSTR_EMAIL_DOWNLOADS", "Hvorvidt filene definert over skal sendes eller ikke n√•r kunden bestiller produktet (dette vil skje n√•r bestillingen opprettes - s√• hvis du har \"Vent inntil betalt\" satt til \"Ja\" p√• bestillingsskjemaet, vil de ikke f√• filene f√∏r de har betalt for de).");
 define("NBILL_EMAIL_DOWNLOADS_MESSAGE", "E-post melding");
 define("NBILL_INSTR_EMAIL_DOWNLOADS_MESSAGE", "Skriv inn meldingen du vil sende sammen med vedleggene (Gjelder bare hvis \"E-post nedlastning?\" er satt til \"Ja\").");
-define("NBILL_NET_PRICE_SEMI_ANNUALLY", "Nettopris halvÂrlig (6 mÂneder)");
-define("NBILL_INSTR_NET_PRICE_SEMI_ANNUALLY", "Hvis produktet kan betales for halvÂrlig (hver 6. mÂned), angi nettopris pr. 6 mÂneder.");
+define("NBILL_NET_PRICE_SEMI_ANNUALLY", "Nettopris halv√•rlig (6 m√•neder)");
+define("NBILL_INSTR_NET_PRICE_SEMI_ANNUALLY", "Hvis produktet kan betales for halv√•rlig (hver 6. m√•ned), angi nettopris pr. 6 m√•neder.");
 
 /****************/
 /* Version 1.1.4
@@ -136,9 +136,8 @@ define("NBILL_INSTR_NET_PRICE_FOUR_WEEKLY", "Hvis produktet kan betales for hver
 /* Note to translators: Text has changed on the following constants:
 /* NBILL_PRODUCT_DISCOUNT_INTRO, NBILL_PRODUCT_DISCOUNT_QTY (lines 103 and 106 in the English file)
 /**************/
-define("NBILL_PRODUCT_UPDATE_EXISTING_ORDERS", "…n eller flere eksisterende ordre som er fornyet manuelt ble funnet for dette produktet.Klikk \"OK\" hvis du vil oppdatere prisene pÂ eksisterende bestilling(er).");
+define("NBILL_PRODUCT_UPDATE_EXISTING_ORDERS", "√ân eller flere eksisterende ordre som er fornyet manuelt ble funnet for dette produktet.Klikk \"OK\" hvis du vil oppdatere prisene p√• eksisterende bestilling(er).");
 define("NBILL_EXISTING_ORDERS_UPDATED", "%s ordre(r) er oppdatert.");
-define("NBILL_PRODUCT_DOWNLOADABLE_TOKENS", "Du kan bruke f¯lgende frase for Â lage \"likelydende\" filnavn: ##ordre_no##, ##ordre_id##, ##kunde_id##. Eks: Hvis du oppretter en fil for hver bestilling kan du bruke et filnavn som \"/hjem/brukernavn/fil##ordre_no##.txt\" - ##ordre_no## frasen vil bli erstattet av det faktiske bestillingsnummeret. Samme prinsipp kan brukes pÂ alle 3 nedlastningslenkene.");
+define("NBILL_PRODUCT_DOWNLOADABLE_TOKENS", "Du kan bruke f√∏lgende frase for √• lage \"likelydende\" filnavn: ##ordre_no##, ##ordre_id##, ##kunde_id##. Eks: Hvis du oppretter en fil for hver bestilling kan du bruke et filnavn som \"/hjem/brukernavn/fil##ordre_no##.txt\" - ##ordre_no## frasen vil bli erstattet av det faktiske bestillingsnummeret. Samme prinsipp kan brukes p√• alle 3 nedlastningslenkene.");
 define("NBILL_PRODUCT_DISCOUNT_MULTIPLY", "Multipliser med antallet?");
-define("NBILL_PRODUCT_DISCOUNT_OFFSET", "Forskj¯vet");
-?>
+define("NBILL_PRODUCT_DISCOUNT_OFFSET", "Forskj√∏vet");
