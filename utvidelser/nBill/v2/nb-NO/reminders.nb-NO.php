1f554a53ff865486e59979514ccfe70e35ab584a
roar <roar@6aa871c5-9e2b-0410-bde1-d651c692654a>
2010-05-15 22:58:06 +0000

    Klar for korrektur

--- utvidelser/nBill/v2/nb-NO/reminders.nb-NO.php
+++ utvidelser/nBill/v2/nb-NO/reminders.nb-NO.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Language file for nBill
-* @version 1
+* Language file for the Reminders feature
+* @version 2
 * @package nBill
 * @copyright (C) 2006-2009 Netshine Software Limited
 * @license http://www.nbill.co.uk/eula.html
@@ -23,54 +23,54 @@
 (defined('_VALID_MOS') || defined('_JEXEC') || defined('NBILL_VALID_NBF')) or die('Access Denied.');
 
 //Reminders
-define("NBILL_REMINDERS_TITLE", "PÂminnelser");
-define("NBILL_REMINDERS_WARNING", "Denne funksjonen har potensial til Â sende e-post til ALLE dine kunder. VÊr forsiktig med masseutsendelse!");
-define("NBILL_REMINDERS_INTRO", "Dine pÂminnelser som du vil sende til deg selv eller til dine kunder nÂr utbetalingene forfaller eller ordre skal fornyes, eller er i ferd med Â utl¯pe, eller nÂr fakturaer ikke er betalt. MERK: Disse innstillingene har INGEN effekt med mindre du ogsÂ har satt opp en planlagt oppgave til Â sjekke hva som er forfalt og sÂ sende en e-post. For Â gj¯re dette vil du vanligvis bruke en CRON-jobb sammen med ett skript, planlagt pÂminnelse - som er tilgjengelig fra " . NBILL_BRANDING_NAME . " nettside. Vennligst se dokumentasjonen for mer informasjon om hvordan du setter opp en CRON-jobb.");
-define("NBILL_REMINDER_NAME", "PÂminnelsesnavn");
-define("NBILL_EDIT_REMINDER", "Rediger pÂminnelse");
-define("NBILL_REMINDER_TYPE", "Type pÂminnelse");
+define("NBILL_REMINDERS_TITLE", "P√•minnelser");
+define("NBILL_REMINDERS_WARNING", "Denne funksjonen har potensial til √• sende e-post til ALLE dine kunder. V√¶r forsiktig med masseutsendelse!");
+define("NBILL_REMINDERS_INTRO", "Dine p√•minnelser som du vil sende til deg selv eller til dine kunder n√•r utbetalingene forfaller eller ordre skal fornyes, eller er i ferd med √• utl√∏pe, eller n√•r fakturaer ikke er betalt. MERK: Disse innstillingene har INGEN effekt med mindre du ogs√• har satt opp en planlagt oppgave til √• sjekke hva som er forfalt og s√• sende en e-post. For √• gj√∏re dette vil du vanligvis bruke en CRON-jobb sammen med ett skript, planlagt p√•minnelse - som er tilgjengelig fra " . NBILL_BRANDING_NAME . " nettside. Vennligst se dokumentasjonen for mer informasjon om hvordan du setter opp en CRON-jobb.");
+define("NBILL_REMINDER_NAME", "P√•minnelsesnavn");
+define("NBILL_EDIT_REMINDER", "Rediger p√•minnelse");
+define("NBILL_REMINDER_TYPE", "Type p√•minnelse");
 define("NBILL_REMINDER_ACTIVE", "Aktiv?");
 define("NBILL_REMINDER_ACTIVE_YES", "Aktive");
 define("NBILL_REMINDER_ACTIVE_NO", "Inaktiv");
-define("NBILL_REMINDER_NAME_REQUIRED", "Skriv inn et navn for Â identifisere denne pÂminnelsen");
+define("NBILL_REMINDER_NAME_REQUIRED", "Skriv inn et navn for √• identifisere denne p√•minnelsen");
 define("NBILL_REMINDER_STARTING_FROM", "Fra");
 define("NBILL_REMINDER_NUMBER_OF_UNITS", "Antall enheter");
-define("NBILL_NEW_REMINDER", "Ny pÂminnelse");
-define("NBILL_REMINDER_DETAILS", "PÂminnelsesdetaljer");
-define("NBILL_INSTR_REMINDER_NAME", "Skriv inn et navn som gj¯r det lett Â identifisere hva denne pÂminnelsen gjelder.");
-define("NBILL_INSTR_REMINDER_TYPE", "Velg type pÂminnelse du vil lage.");
-define("NBILL_INSTR_REMINDER_ACTIVE", "Skal sendes e-post for denne pÂminnelsen?");
+define("NBILL_NEW_REMINDER", "Ny p√•minnelse");
+define("NBILL_REMINDER_DETAILS", "P√•minnelsesdetaljer");
+define("NBILL_INSTR_REMINDER_NAME", "Skriv inn et navn som gj√∏r det lett √• identifisere hva denne p√•minnelsen gjelder.");
+define("NBILL_INSTR_REMINDER_TYPE", "Velg type p√•minnelse du vil lage.");
+define("NBILL_INSTR_REMINDER_ACTIVE", "Hvorvidt det skal sendes e-post for denne p√•minnelsen?");
 define("NBILL_REMINDER_ADMIN", "Administrator?");
-define("NBILL_INSTR_REMINDER_ADMIN", "Hvorvidt pÂminnelsen skal sendes til administrator og ikke til kunden (hvis du ¯nsker pÂminnelse sendt til bÂde administrator og kunde, mÂ du sette opp 2 separate pÂminnelser).");
-define("NBILL_INSTR_REMINDER_STARTING_FROM", "NÂr skal e-post med pÂminnelser starte? Hvis feltet er tomt vil pÂminnelse bli sendt uavhengig av dato).");
+define("NBILL_INSTR_REMINDER_ADMIN", "Hvorvidt p√•minnelsen skal sendes til administrator og ikke til kunden (hvis du √∏nsker p√•minnelse sendt til b√•de administrator og kunde, m√• du sette opp 2 separate p√•minnelser).");
+define("NBILL_INSTR_REMINDER_STARTING_FROM", "N√•r skal e-post med p√•minnelser starte? Hvis feltet er tomt vil p√•minnelse bli sendt uavhengig av dato).");
 define("NBILL_REMINDER_NO_OF_UNITS", "Antall enheter");
-define("NBILL_INSTR_REMINDER_NO_OF_UNITS", "Angi antall enheter for intervallet mellom hendelsen og pÂminnelsen (eller i tilfelle av brukerdefinerte pÂminnelser, intervallet mellom pÂminnelser). Eks: Intervall pÂ 7 dager. Sett tallet til 7 og enheten til 'Dager'.");
+define("NBILL_INSTR_REMINDER_NO_OF_UNITS", "Angi antall enheter for intervallet mellom hendelsen og p√•minnelsen (eller i tilfelle av brukerdefinerte p√•minnelser, intervallet mellom p√•minnelser). Eks: Intervall p√• 7 dager. Sett tallet til 7 og enheten til 'Dager'.");
 define("NBILL_REMINDER_UNITS", "Enhet");
 define("NBILL_INSTR_REMINDER_UNITS", "Velg type enhet for dette intervallet.");
 define("NBILL_REMINDER_SEND_AFTER", "Send etter?");
-define("NBILL_INSTR_REMINDER_SEND_AFTER", "Skal det sendes pÂminnelse etter hendelsen? Vanligvis bare brukt pÂ forfalte fakturaer - dvs. Â sende en pÂminnelse x dager etter forfallsdato pÂ faktura).");
+define("NBILL_INSTR_REMINDER_SEND_AFTER", "Hvorvidt det skal det sendes p√•minnelse etter hendelsen? Vanligvis bare brukt p√• forfalte fakturaer - dvs. √• sende en p√•minnelse x dager etter forfallsdato p√• faktura).");
 define("NBILL_REMINDER_EMAIL_TEXT", "E-post tekst");
-define("NBILL_INSTR_REMINDER_EMAIL_TEXT", "Skriv inn e-postmeldingen du ¯nsker Â sende. Du kan bruke f¯lgende frase: {NAME} = setter inn kundens navn; {VENDOR} = setter inn navn pÂ leverand¯r; {DATE} = setter inn dato; {INTERVAL} = setter inn antall; {UNITS} = setter inn enhet; {ORDER} = ordredetaljer; {AMOUNT} = utestÂende bel¯p  (NB. rabatter vil IKKE bli tatt med); {WEBSITE} = din nettsides adresse; {PAYLINK} = setter inn en lenke til betaling hvis det er aktuelt (fornyelser og forfalte fakturaer bare); {PAYLINK_HTTPS} = samme som 'paylink', men bruker https i starten av nettadressen; {INVOICE} = setter inn fakturanr. (bare for forfalte fakturaer).");
+define("NBILL_INSTR_REMINDER_EMAIL_TEXT", "Skriv inn e-postmeldingen du √∏nsker √• sende. Du kan bruke f√∏lgende frase: {NAME} = setter inn kundens navn; {VENDOR} = setter inn navn p√• leverand√∏r; {DATE} = setter inn dato; {INTERVAL} = setter inn antall; {UNITS} = setter inn enhet; {ORDER} = ordredetaljer; {AMOUNT} = utest√•ende bel√∏p  (NB. rabatter vil IKKE bli tatt med); {WEBSITE} = din nettsides adresse; {PAYLINK} = setter inn en lenke til betaling hvis det er aktuelt (fornyelser og forfalte fakturaer bare); {PAYLINK_HTTPS} = samme som 'paylink', men bruker https i starten av nettadressen; {INVOICE} = setter inn fakturanr. (bare for forfalte fakturaer).");
 define("NBILL_REMINDER_CLIENT", "Kunde");
-define("NBILL_INSTR_REMINDER_CLIENT", "Kunde som pÂminnelsen skal sendes til (hvis til 'Administrator' - settes innstillingen ovenfor til 'Nei'). ADVARSEL! Hvis dette er satt til 'Gjelder ikke', vil en pÂminnelse bli sendt til ALLE kundene som har ett forfall. Hvis dette er en 'brukerdefinert' pÂminnelse, og 'Administrator' innstillingen er satt til 'Nei', mÂ du velge en kunde her, ellers vil ingen pÂminnelse blir sendt.");
+define("NBILL_INSTR_REMINDER_CLIENT", "Kunde som p√•minnelsen skal sendes til (hvis til 'Administrator' - settes innstillingen ovenfor til 'Nei'). ADVARSEL! Hvis dette er satt til 'Gjelder ikke', vil en p√•minnelse bli sendt til ALLE kundene som har ett forfall. Hvis dette er en 'brukerdefinert' p√•minnelse, og 'Administrator' innstillingen er satt til 'Nei', m√• du velge en kunde her, ellers vil ingen p√•minnelse blir sendt.");
 define("NBILL_REMINDER_FILTER", "Filter");
-define("NBILL_INSTR_REMINDER_FILTER", "BARE FOR AVANSERTE BRUKERE: Du kan filtrere postene som pÂminnelsen vil gjelde for ved Â legge inn et filter her i form av en SQL-sp¯rring (where-setning). F.eks, Â sende pÂminnelser bare til kunder med bestillinger som har Ârlige betalings intervaller, Du kan legge inn \" #__inv_orders.payment_frequency = 'EE' \" her. Databasetabeller som filtrering kan utf¯res pÂ avhenger av typen pÂminnelse.");
+define("NBILL_INSTR_REMINDER_FILTER", "BARE FOR AVANSERTE BRUKERE: Du kan filtrere postene som p√•minnelsen vil gjelde for ved √• legge inn et filter her i form av en SQL-sp√∏rring (where-setning). F.eks, √• sende p√•minnelser bare til kunder med bestillinger som har √•rlige betalings intervaller, Du kan legge inn \" #__nbill_orders.payment_frequency = 'EE' \" her. Databasetabeller som filtrering kan utf√∏res p√• avhenger av typen p√•minnelse.");
 
 //Default e-mail text
-define("NBILL_REMINDER_PAYMENT_DUE_EMAIL", "Hei {NAME},\n\nDette er en e-post for Â meddele at en periodisk betaling for f¯lgende ordre forfaller den {DATE}. Hvis du har angitt en periodisk betaling, vil du automatisk bli belastet med {AMOUNT} (fratrukket eventuelle rabatter om du er kvalifisert til det) pÂ eller rundt forfallsdato.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger ¯nsker Â motta disse pÂminnelsene, mÂ du logge inn pÂ din konto pÂ {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}");
-define("NBILL_REMINDER_ORDER_EXPIRY_EMAIL", "Hei {NAME},\n\nDette er en e-post for Â meddele at f¯lgende ordre utl¯per {DATE}. PÂse at eventuelle betalingsplaner er annullert.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger ¯nsker Â motta disse pÂminnelsene, mÂ du logge inn pÂ din konto pÂ {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}.");
-define("NBILL_REMINDER_RENEWAL_DUE_EMAIL", "Hei {NAME},\n\nDette er en pÂminnelse om at abonnementet for f¯lgende ordre forfaller pÂ {DATE}. Hvis du ¯nsker Â fornye, kan du bruke f¯lgende link: {PAYLINK}. Eventuell gjenvÊrende tid pÂ det gamle abonnementet vil bli lagt til pÂ det nye slik at du ikke taper ved Â fornye f¯r abonnementet utl¯per.\n\n{ORDER}\n\nHvis du ikke lenger ¯nsker Â motta disse pÂminnelsene, mÂ du logge inn pÂ din konto pÂ {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}");
-define("NBILL_REMINDER_INVOICE_OVERDUE_EMAIL", "Hei {NAME},\n\nDette er en pÂminnelse om at fakturanummer {INVOICE} for {AMOUNT} har vÊrt utestÂende i {INTERVAL} {UNITS}, og er derfor nÂ forfalt. Vennligst betal denne fakturaen sÂ snart som mulig. Hvis du ¯nsker Â betale elektronisk, kan du bruke f¯lgende lenke: {PAYLINK}.\n\nHilsen,\n{VENDOR}.");
+define("NBILL_REMINDER_PAYMENT_DUE_EMAIL", "Hei {NAME},\n\nDette er en e-post for √• meddele at en periodisk betaling for f√∏lgende ordre forfaller den {DATE}. Hvis du har angitt en periodisk betaling, vil du automatisk bli belastet med {AMOUNT} (fratrukket eventuelle rabatter om du er kvalifisert til det) p√• eller rundt forfallsdato.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger √∏nsker √• motta disse p√•minnelsene, m√• du logge inn p√• din konto p√• {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}");
+define("NBILL_REMINDER_ORDER_EXPIRY_EMAIL", "Hei {NAME},\n\nDette er en e-post for √• meddele at f√∏lgende ordre utl√∏per {DATE}. P√•se at eventuelle betalingsplaner er annullert.\n\nOrdredetaljer:\n\n{ORDER}\n\nHvis du ikke lenger √∏nsker √• motta disse p√•minnelsene, m√• du logge inn p√• din konto p√• {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}.");
+define("NBILL_REMINDER_RENEWAL_DUE_EMAIL", "Hei {NAME},\n\nDette er en p√•minnelse om at abonnementet for f√∏lgende ordre forfaller p√• {DATE}. Hvis du √∏nsker √• fornye, kan du bruke f√∏lgende link: {PAYLINK}. Eventuell gjenv√¶rende tid p√• det gamle abonnementet vil bli lagt til p√• det nye slik at du ikke taper ved √• fornye f√∏r abonnementet utl√∏per.\n\n{ORDER}\n\nHvis du ikke lenger √∏nsker √• motta disse p√•minnelsene, m√• du logge inn p√• din konto p√• {WEBSITE}, og endre innstillinger under \"Min konto-> Min profil\".\n\nHilsen,\n{VENDOR}");
+define("NBILL_REMINDER_INVOICE_OVERDUE_EMAIL", "Hei {NAME},\n\nDette er en p√•minnelse om at fakturanummer {INVOICE} for {AMOUNT} har v√¶rt utest√•ende i {INTERVAL} {UNITS}, og er derfor n√• forfalt. Vennligst betal denne fakturaen s√• snart som mulig. Hvis du √∏nsker √• betale elektronisk, kan du bruke f√∏lgende lenke: {PAYLINK}.\n\nHilsen,\n{VENDOR}.");
 define("NBILL_REMINDER_USER_DEFINED_EMAIL", "Ikke glemt: <Skriv inn tekst her!>");
-define("NBILL_REMINDER_PAYMENT_DUE_SUBJECT", "PÂminnelse: Betaling forfalt");
-define("NBILL_REMINDER_ORDER_EXPIRY_SUBJECT", "PÂminnelse: Ordre utl¯pt");
-define("NBILL_REMINDER_RENEWAL_DUE_SUBJECT", "PÂminnelse: Fornyelse forfalt");
-define("NBILL_REMINDER_INVOICE_OVERDUE_SUBJECT", "PÂminnelse: Faktura forfalt");
-define("NBILL_REMINDER_USER_DEFINED_SUBJECT", "PÂminnelse");
+define("NBILL_REMINDER_PAYMENT_DUE_SUBJECT", "P√•minnelse: Betaling forfalt");
+define("NBILL_REMINDER_ORDER_EXPIRY_SUBJECT", "P√•minnelse: Ordre utl√∏pt");
+define("NBILL_REMINDER_RENEWAL_DUE_SUBJECT", "P√•minnelse: Fornyelse forfalt");
+define("NBILL_REMINDER_INVOICE_OVERDUE_SUBJECT", "P√•minnelse: Faktura forfalt");
+define("NBILL_REMINDER_USER_DEFINED_SUBJECT", "P√•minnelse");
 
 //Reminder Types
 define("NBILL_REMINDER_PAYMENT_DUE", "Betaling forfalt");
-define("NBILL_REMINDER_ORDER_EXPIRY", "Ordre utl¯pt");
+define("NBILL_REMINDER_ORDER_EXPIRY", "Ordre utl√∏pt");
 define("NBILL_REMINDER_RENEWAL_DUE", "Fornyelse forfalt");
 define("NBILL_REMINDER_INVOICE_OVERDUE", "Faktura forfalt");
 define("NBILL_REMINDER_USER_DEFINED", "Brukerdefinert");
@@ -78,21 +78,20 @@ define("NBILL_REMINDER_USER_DEFINED", "Brukerdefinert");
 //Units
 define("NBILL_REMINDER_UNIT_DAYS", "Dager");
 define("NBILL_REMINDER_UNIT_WEEKS", "Uker");
-define("NBILL_REMINDER_UNIT_MONTHS", "MÂneder");
+define("NBILL_REMINDER_UNIT_MONTHS", "M√•neder");
 
 //CRON script
-define("NBILL_REMINDER", "PÂminnelse");
-define("NBILL_REMINDERS_SENT_INTRO", "%s PÂminnelser er behandlet av " . NBILL_BRANDING_NAME . ". Tid: %s.\n\n");
-define("NBILL_REMINDERS_SENT_SUBJECT", NBILL_BRANDING_NAME . " Planlagt pÂminnelse");
+define("NBILL_REMINDER", "P√•minnelse");
+define("NBILL_REMINDERS_SENT_INTRO", "%s P√•minnelser er behandlet av " . NBILL_BRANDING_NAME . ". Tid: %s.\n\n");
+define("NBILL_REMINDERS_SENT_SUBJECT", NBILL_BRANDING_NAME . " Planlagt p√•minnelse");
 define("NBILL_REMINDERS_SENT_FAILURE", "FEIL");
 define("NBILL_REMINDERS_SENT_NO_LICENSE", NBILL_BRANDING_NAME . " Lisenskoden er feil eller ble ikke funnet");
 define("NBILL_REMINDERS_SENT_TO", "sendt til");
 define("NBILL_REMINDERS_SENT_FOR","for kunde");
 define("NBILL_REMINDERS_SENT_MESSAGE", "MELDING:");
-define("NBILL_REMINDER_SENT_ERROR", "ADVARSEL! Det oppstod en feil ved fors¯k pÂ Â sende denne pÂminnelsen. PÂminnelse er ikke sendt.");
-define("NBILL_REMINDER_SENT_ERROR_NO_CLIENT", "ADVARSEL! Det oppstod en feil ved fors¯k pÂ Â sende denne pÂminnelsen. Ingen kunde poster ble funnet. PÂminnelse er ikke sendt.");
+define("NBILL_REMINDER_SENT_ERROR", "ADVARSEL! Det oppstod en feil ved fors√∏k p√• √• sende denne p√•minnelsen. P√•minnelse er ikke sendt.");
+define("NBILL_REMINDER_SENT_ERROR_NO_CLIENT", "ADVARSEL! Det oppstod en feil ved fors√∏k p√• √• sende denne p√•minnelsen. Ingen kunder ble funnet. P√•minnelse er ikke sendt.");
 define("NBILL_REMINDER_ORDER_NO", "Ordrenr.");
 
 //Version 1.2.1
 //Note to translators: Text for NBILL_INSTR_REMINDER_EMAIL_TEXT updated to add description of {PAYLINK_HTTPS} (on line 51 of English language file)
-?>
--- utvidelser/nBill/v2/nb-NO/shipping.nb-NO.php
+++ utvidelser/nBill/v2/nb-NO/shipping.nb-NO.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Language file for nBill
-* @version 1
+* Language file for the Shipping feature.
+* @version 2
 * @package nBill
 * @copyright (C) 2006-2009 Netshine Software Limited
 * @license http://www.nbill.co.uk/eula.html
@@ -22,32 +22,31 @@
 //Ensure this file has been reached through a valid entry point (not always necessary eg. for class files, but included on every file to be safe!)
 (defined('_VALID_MOS') || defined('_JEXEC') || defined('NBILL_VALID_NBF')) or die('Access Denied.');
 
-//Carriage
-define("NBILL_CARRIAGE_TITLE", "Fraktsatser");
-define("NBILL_CARRIAGE_INTRO", "Dette er valgfritt. Hvis du legger til fraktkostnader til prisen pÂ produktene dine, kan du angi satsene her. Du kan ogsÂ angi om transporten er MVA-pliktig, og i sÂ fall bruke en annen sats av MVA for transportdelen av fakturaen.");
-define("NBILL_CARRIAGE_COUNTRY", "Land");
-define("NBILL_CARRIAGE_PRICE_PER_UNIT", "Enhetspris");
-define("NBILL_EDIT_CARRIAGE_RATE", "Rediger fraktsatser");
-define("NBILL_NEW_CARRIAGE_RATE", "Ny fraktsats");
-define("NBILL_SERVICE_NAME_REQUIRED", "Fyll inn navnet pÂ denne tjenesten");
+//Shipping
+define("NBILL_SHIPPING_TITLE", "Fraktsatser");
+define("NBILL_SHIPPING_INTRO", "Dette er valgfritt. Hvis du legger til fraktkostnader til prisen p√• produktene dine, kan du angi satsene her. Du kan ogs√• angi om transporten er MVA-pliktig, og i s√• fall bruke en annen sats av MVA for transportdelen av fakturaen.");
+define("NBILL_SHIPPING_COUNTRY", "Land");
+define("NBILL_SHIPPING_PRICE_PER_UNIT", "Enhetspris");
+define("NBILL_EDIT_SHIPPING_RATE", "Rediger fraktsatser");
+define("NBILL_NEW_SHIPPING_RATE", "Ny fraktsats");
+define("NBILL_SERVICE_NAME_REQUIRED", "Fyll inn navnet p√• denne tjenesten");
 define("NBILL_COUNTRY_REQUIRED", "Velg land hvor denne tjenesten gjelder");
-define("NBILL_CARRIAGE_DETAILS", "Fraktdetaljer");
-define("NBILL_CARRIAGE_CODE", "Kode");
+define("NBILL_SHIPPING_DETAILS", "Fraktdetaljer");
+define("NBILL_SHIPPING_CODE", "Kode");
 define("NBILL_NET_PRICE_PER_UNIT", "Nettopris pr. enhet");
-define("NBILL_CARRIAGE_FIXED", "Fastpris?");
-define("NBILL_CARRIAGE_IS_TAXABLE", "Er frakten MVA plikig?");
-define("NBILL_CARRIAGE_TAX_RATE", "MVA-sats om forskjellig");
-define("NBILL_INSTR_CARRIAGE_SERVICE", "Skriv inn et beskrivende navn for tjenesten (For eksempel 'Ekspress over natten', 'Levering neste dag')");
-define("NBILL_INSTR_CARRIAGE_CODE", "Angi din egen kode for denne tjenesten (valgfritt, men brukes til Â sortere leveringsmuligheter alfabetisk).");
-define("NBILL_INSTR_CARRIAGE_COUNTRY", "Velg land der denne tjenesten gjelder.");
-define("NBILL_INSTR_NET_PRICE_PER_UNIT", "Hvis du har betalt fastpris for transport, angir du bel¯pet her og velger \"Ja\" pÂ fastpris-alternativet nedenfor. Hvis fraktprisen avhenger av produktets st¯rrelse, tyngde eller mengde, skriver du bel¯pet som representerer den minste fellesnevneren for produktenes fraktavgift. Deretter kan du angi en multiplikator pÂ denne verdien som vil bli brukt til Â beregne transportavgifter som vises pÂ fakturaene. Ikke inkluder valutasymbol.");
-define("NBILL_INSTR_CARRIAGE_FIXED", "Hvis du har betalt fastpris per faktura for transport, velger du \"Ja\", og verdien du skrev inn i \"Nettopris pr enhet\" vil bli brukt som et fast gebyr for hele fakturaen (i stedet for som en enhet). Hvis fraktprisen avhenger av produktets st¯rrelse, tyngde eller mengde, velger du \"Nei\".");
-define("NBILL_INSTR_CARRIAGE_IS_TAXABLE", "IndikÈr om det skal betales moms av fraktprisen");
-define("NBILL_INSTR_CARRIAGE_TAX_RATE", "Hvis transporten er MVA-pliktig OG avgiften for transport er annerledels enn til vanlig, skriver du inn transportens MVA-sats her (i prosent, men uten prosenttegn %)");
-define("NBILL_CARRIAGE_PRICE_INTRO", "Angi prisen for transport for hver valuta som du vil selge i.");
-define("NBILL_CARRIAGE_ID", "ID");
+define("NBILL_SHIPPING_FIXED", "Fastpris?");
+define("NBILL_SHIPPING_IS_TAXABLE", "Er frakten MVA pliktig?");
+define("NBILL_SHIPPING_TAX_RATE", "MVA-sats om forskjellig");
+define("NBILL_INSTR_SHIPPING_SERVICE", "Skriv inn et beskrivende navn for tjenesten (For eksempel 'Ekspress over natten', 'Levering neste dag')");
+define("NBILL_INSTR_SHIPPING_CODE", "Angi din egen kode for denne tjenesten (valgfritt, men brukes til √• sortere leveringsmuligheter alfabetisk).");
+define("NBILL_INSTR_SHIPPING_COUNTRY", "Velg land der denne tjenesten gjelder.");
+define("NBILL_INSTR_NET_PRICE_PER_UNIT", "Hvis du har betalt fastpris for transport, angir du bel√∏pet her og velger \"Ja\" p√• fastpris-alternativet nedenfor. Hvis fraktprisen avhenger av produktets st√∏rrelse, tyngde eller mengde, skriver du bel√∏pet som representerer den minste fellesnevneren for produktenes fraktavgift. Deretter kan du angi en multiplikator p√• denne verdien som vil bli brukt til √• beregne transportavgifter som vises p√• fakturaene. Ikke inkluder valutasymbol.");
+define("NBILL_INSTR_SHIPPING_FIXED", "Hvis du har betalt fastpris per faktura for transport, velger du \"Ja\", og verdien du skrev inn i \"Nettopris pr enhet\" vil bli brukt som et fast gebyr for hele fakturaen (i stedet for som en enhet). Hvis fraktprisen avhenger av produktets st√∏rrelse, tyngde eller mengde, velger du \"Nei\".");
+define("NBILL_INSTR_SHIPPING_IS_TAXABLE", "Indik√©r om det skal betales moms av fraktprisen");
+define("NBILL_INSTR_SHIPPING_TAX_RATE", "Hvis transporten er MVA-pliktig OG avgiften for transport er annerledes enn til vanlig, skriver du inn transportens MVA-sats her (i prosent, men uten prosenttegn %)");
+define("NBILL_SHIPPING_PRICE_INTRO", "Angi prisen for transport for hver valuta som du vil selge i.");
+define("NBILL_SHIPPING_ID", "ID");
 
 //Version 1.2.0
-define("NBILL_CARRIAGE_TRACKING_URL", "Transportens nettadresse for sporing");
-define("NBILL_INSTR_CARRIAGE_TRACKING_URL", "Hvis transport¯ren tillater elektronisk sporing av pakker, angi nettadressen her, og bruk ## (dobbel hash) som plassholder for sporingsnummer. NÂr du sender pakken kan du angi det riktige sporingsnummeret pÂ ordren. Denne vil da bli slÂtt sammen med nettadressen registrert her, for Â generere en lenke pÂ nettsidens brukerside som gj¯r det mulig for brukeren Â spore sin forsendelse (om dette lar seg gj¯re i henhold til visningsalternativene).");
-?>
+define("NBILL_SHIPPING_TRACKING_URL", "Transportens nettadresse for sporing");
+define("NBILL_INSTR_SHIPPING_TRACKING_URL", "Hvis transport√∏ren tillater elektronisk sporing av pakker, angi nettadressen her, og bruk ## (dobbel hash) som plassholder for sporingsnummer. N√•r du sender pakken kan du angi det riktige sporingsnummeret p√• ordren. Denne vil da bli sl√•tt sammen med nettadressen registrert her, for √• generere en lenke p√• nettsidens brukerside som gj√∏r det mulig for brukeren √• spore sin forsendelse (om dette lar seg gj√∏re i henhold til visningsalternativene).");
