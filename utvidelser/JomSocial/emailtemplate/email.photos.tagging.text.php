79a576e8ce494434859e71928d3b681d5ea5cf3a
tommyrsk <tommyrsk@6aa871c5-9e2b-0410-bde1-d651c692654a>
2010-08-02 11:50:33 +0000

    Oppdatering til 1.8.7 (lagt til filene for e-postmalene i Engelsk form for å kunne spore eventuelle endringer)

---
+++ utvidelser/JomSocial/emailtemplate/email.photos.tagging.text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		JomSocial
+ * @subpackage 	Template 
+ * @copyright (C) 2008 by Slashes & Dots Sdn Bhd - All rights reserved!
+ * @license		GNU/GPL, see LICENSE.php
+ */
+defined('_JEXEC') or die();
+?>
+Hi {target},
+
+{actor} tagged you in a photo.  To view this photo, please click the link below:
+
+{url}
+
+
+Have a nice day!
---
+++ utvidelser/JomSocial/emailtemplate/email.photos.wall.html.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package		JomSocial
+ * @subpackage 	Template 
+ * @copyright (C) 2008 by Slashes & Dots Sdn Bhd - All rights reserved!
+ * @license		GNU/GPL, see LICENSE.php
+ */
+defined('_JEXEC') or die();
+?>
+Hi {target},
+
+{actor} just posted a wall message. 
+
+Message:
+
+<?php echo $message; ?>
+
+You can read the message at:
+
+
+<a href="{url}">{url}</a>
+
+
+Have a nice day!
+
