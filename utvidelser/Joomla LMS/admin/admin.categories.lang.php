ad21474dcb0c6a5401b2f7c1276a4eafc2c5d827
kncarlsen <kncarlsen@6aa871c5-9e2b-0410-bde1-d651c692654a>
2010-11-24 19:10:29 +0000

    Ferdig oversatt

--- utvidelser/Joomla LMS/admin/admin.categories.lang.php
+++ utvidelser/Joomla LMS/admin/admin.categories.lang.php
@@ -7,25 +7,25 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-$JLMS_LANGUAGE['_JLMS_FLMS_CATS_MANG'] = 'Categories management';
-$JLMS_LANGUAGE['_JLMS_FLMS_MAX_LEVLS'] = 'Max Levels';
-$JLMS_LANGUAGE['_JLMS_FLMS_MSG_CAT_MU_HAVE_N'] = 'Category must have a name';
-$JLMS_LANGUAGE['_JLMS_FLMS_EDIT_CAT'] = 'Edit category';
-$JLMS_LANGUAGE['_JLMS_FLMS_ADD_CAT'] = 'Add category';
-$JLMS_LANGUAGE['_JLMS_FLMS_PARENT_ITEM'] = 'Parent Item';
-$JLMS_LANGUAGE['_JLMS_FLMS_LESSON_TYPE'] = 'Lesson Type';
-$JLMS_LANGUAGE['_JLMS_FLMS_RESTR_CAT'] = 'Restricted category';
-$JLMS_LANGUAGE['_JLMS_FLMS_RESTR_GRPS'] = 'Restricted groups';
-$JLMS_LANGUAGE['_JLMS_FLMS_MSG_USE_PERM'] = 'To use category permissions management, enable "global groups" mode in JoomlaLMS configuration first.';
-$JLMS_LANGUAGE['_JLMS_FLMS_CONF'] = 'Configuration';
-$JLMS_LANGUAGE['_JLMS_FLMS_TOP'] = 'Top';
+$JLMS_LANGUAGE['_JLMS_FLMS_CATS_MANG'] = 'Kategoribehandling';
+$JLMS_LANGUAGE['_JLMS_FLMS_MAX_LEVLS'] = 'Maks. antall nivåer';
+$JLMS_LANGUAGE['_JLMS_FLMS_MSG_CAT_MU_HAVE_N'] = 'Kategorien må ha et navn';
+$JLMS_LANGUAGE['_JLMS_FLMS_EDIT_CAT'] = 'Rediger kategori';
+$JLMS_LANGUAGE['_JLMS_FLMS_ADD_CAT'] = 'Opprett kategori';
+$JLMS_LANGUAGE['_JLMS_FLMS_PARENT_ITEM'] = 'Overordnet element';
+$JLMS_LANGUAGE['_JLMS_FLMS_LESSON_TYPE'] = 'Leksjonstype';
+$JLMS_LANGUAGE['_JLMS_FLMS_RESTR_CAT'] = 'Kategori med restriksjoner';
+$JLMS_LANGUAGE['_JLMS_FLMS_RESTR_GRPS'] = 'Gruppe med restriksjoner';
+$JLMS_LANGUAGE['_JLMS_FLMS_MSG_USE_PERM'] = 'For å administrere kategorier med restriksjoner, aktiver "global groups"-modus i JoomlaLMS-konfigurasjonen først.';
+$JLMS_LANGUAGE['_JLMS_FLMS_CONF'] = 'Konfigurasjon';
+$JLMS_LANGUAGE['_JLMS_FLMS_TOP'] = 'Topp';
 
-$JLMS_LANGUAGE['_JLMS_LF_MSG_CAT_I_SAVED'] = 'Category item saved';
-$JLMS_LANGUAGE['_JLMS_LF_MSG_CAT_I_DELETED'] = 'Category item deleted';
-$JLMS_LANGUAGE['_JLMS_LF_MSG_CONFIG_SAVED'] = 'Configuration saved';
-$JLMS_LANGUAGE['_JLMS_LF_CRS_CATS'] = 'Course categories:';
-$JLMS_LANGUAGE['_JLMS_LF_SLCT_CAT'] = '- Select Category -';
-$JLMS_LANGUAGE['_JLMS_LF_MSG_ORDER_SAVED'] = 'New ordering saved';
-$JLMS_LANGUAGE['_JLMS_LF_TOP'] = 'Top';
+$JLMS_LANGUAGE['_JLMS_LF_MSG_CAT_I_SAVED'] = 'Kategorielement lagret';
+$JLMS_LANGUAGE['_JLMS_LF_MSG_CAT_I_DELETED'] = 'Kategorielement slettet';
+$JLMS_LANGUAGE['_JLMS_LF_MSG_CONFIG_SAVED'] = 'Konfigurasjon lagret.';
+$JLMS_LANGUAGE['_JLMS_LF_CRS_CATS'] = 'Kurskategorier:';
+$JLMS_LANGUAGE['_JLMS_LF_SLCT_CAT'] = '- Velg kategori -';
+$JLMS_LANGUAGE['_JLMS_LF_MSG_ORDER_SAVED'] = 'Ny sortering lagret';
+$JLMS_LANGUAGE['_JLMS_LF_TOP'] = 'Topp';
 
 ?>
