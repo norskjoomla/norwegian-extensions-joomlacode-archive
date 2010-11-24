51202abb079b3db67e174a59cf43b32bcb7b8a94
kncarlsen <kncarlsen@users.noreply.github.com>
2010-11-24 19:18:23 +0000

    Ferdig oversatt

--- utvidelser/Joomla LMS/admin/admin.menumanager.lang.php
+++ utvidelser/Joomla LMS/admin/admin.menumanager.lang.php
@@ -7,21 +7,21 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-$JLMS_LANGUAGE['_JLMS_MENUM'] = 'Menu manager';
-$JLMS_LANGUAGE['_JLMS_MENUM_GUEST_M'] = '[ Guest menu ]';
-$JLMS_LANGUAGE['_JLMS_MENUM_HOMEPAGE_M'] = '[ Homepage menu ]';
-$JLMS_LANGUAGE['_JLMS_MENUM_TEACHER_M'] = '[ Teacher menu ]';
-$JLMS_LANGUAGE['_JLMS_MENUM_STUDENT_M'] = '[ Student menu ]';
-$JLMS_LANGUAGE['_JLMS_MENUM_CEO_PARENT_M'] = '[ CEO/Parent menu ]';
-$JLMS_LANGUAGE['_JLMS_MENUM_SELECT_M_TYPE'] = 'Select menu type';
-$JLMS_LANGUAGE['_JLMS_MENUM_M_IMAGE'] = 'Menu image';
-$JLMS_LANGUAGE['_JLMS_MENUM_M_ITEM'] = 'Menu Item';
-$JLMS_LANGUAGE['_JLMS_MENUM_SAVE_ORDER'] = 'Save Order';
-$JLMS_LANGUAGE['_JLMS_MENUM_MSG_MARKED_ITEMS'] = 'Items marked with such icon are disabled in JoomlaLMS configuration or not configured';
-$JLMS_LANGUAGE['_JLMS_MENUM_GUEST_MENU'] = 'Guest menu';
-$JLMS_LANGUAGE['_JLMS_MENUM_HOMEP_MENU'] = 'Homepage menu';
-$JLMS_LANGUAGE['_JLMS_MENUM_TEACHER_MENU'] = 'Teacher menu';
-$JLMS_LANGUAGE['_JLMS_MENUM_STUDENT_MENU'] = 'Student menu';
-$JLMS_LANGUAGE['_JLMS_MENUM_PARENT_MENU'] = 'CEO/Parent menu';
-$JLMS_LANGUAGE['_JLMS_MENUM_MSG_NEW_ORDER_SAVED'] = 'New ordering saved';
+$JLMS_LANGUAGE['_JLMS_MENUM'] = 'Menybehandling';
+$JLMS_LANGUAGE['_JLMS_MENUM_GUEST_M'] = '[ Meny for gjest ]';
+$JLMS_LANGUAGE['_JLMS_MENUM_HOMEPAGE_M'] = '[ Meny for hjemmeside ]';
+$JLMS_LANGUAGE['_JLMS_MENUM_TEACHER_M'] = '[ Meny for lærer ]';
+$JLMS_LANGUAGE['_JLMS_MENUM_STUDENT_M'] = '[ Meny for student ]';
+$JLMS_LANGUAGE['_JLMS_MENUM_CEO_PARENT_M'] = '[ Meny for rektor/foreldre ]';
+$JLMS_LANGUAGE['_JLMS_MENUM_SELECT_M_TYPE'] = 'Velg menytype';
+$JLMS_LANGUAGE['_JLMS_MENUM_M_IMAGE'] = 'Menybilde';
+$JLMS_LANGUAGE['_JLMS_MENUM_M_ITEM'] = 'Menyelement';
+$JLMS_LANGUAGE['_JLMS_MENUM_SAVE_ORDER'] = 'Lagre sortering';
+$JLMS_LANGUAGE['_JLMS_MENUM_MSG_MARKED_ITEMS'] = 'Elementer merket med et slikt ikon er dekativert i JoomlaLMS-konfigurasjonen eller ikke konfigurert';
+$JLMS_LANGUAGE['_JLMS_MENUM_GUEST_MENU'] = 'Meny for gjester';
+$JLMS_LANGUAGE['_JLMS_MENUM_HOMEP_MENU'] = 'Meny for hjemmesiden';
+$JLMS_LANGUAGE['_JLMS_MENUM_TEACHER_MENU'] = 'Meny for læreren';
+$JLMS_LANGUAGE['_JLMS_MENUM_STUDENT_MENU'] = 'Meny for studenten';
+$JLMS_LANGUAGE['_JLMS_MENUM_PARENT_MENU'] = 'Meny for rektor/foreldre';
+$JLMS_LANGUAGE['_JLMS_MENUM_MSG_NEW_ORDER_SAVED'] = 'Ny sortering lagret';
 ?>
