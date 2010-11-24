4018cb2c159f488c51d98c472538f1b18439af6d
kncarlsen <kncarlsen@users.noreply.github.com>
2010-11-24 19:25:02 +0000

    Ferdig oversatt

--- utvidelser/Joomla LMS/admin/admin.about.lang.php
+++ utvidelser/Joomla LMS/admin/admin.about.lang.php
@@ -9,7 +9,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 $JLMS_LANGUAGE['_JLMS_ABOUT_INST_VER'] = 'Installert versjon';
 $JLMS_LANGUAGE['_JLMS_ABOUT_LATEST_VER'] = 'Siste versjon';
-$JLMS_LANGUAGE['_JLMS_ABOUT_LICENSE_EXP_ON'] = 'Lisensen utlÃ¸per den';
+$JLMS_LANGUAGE['_JLMS_ABOUT_LICENSE_EXP_ON'] = 'Lisensen utløper den';
 $JLMS_LANGUAGE['_JLMS_ABOUT_LICENSE_USERS'] = 'Lisensierte brukere';
 $JLMS_LANGUAGE['_JLMS_ABOUT_BRANDING_FREE'] = 'Uten reklame';
 $JLMS_LANGUAGE['_JLMS_ABOUT_MANAGERS_LIST'] = '';
@@ -18,9 +18,9 @@ $JLMS_LANGUAGE['_JLMS_ABOUT_TBR_SUPPORT'] = 'Support';
 
 
 $JLMS_LANGUAGE['_JLMS_SUPP'] = 'Support';
-$JLMS_LANGUAGE['_JLMS_SUPP_IF_HAVE_QUESTS'] = 'Dersom du har spÃ¸rsmÃ¥l/forslag eller opplever problemer med JoomlaLMS, vennligst kontakt vÃ¥rt supportteam - <a href="http://www.joomlalms.com/helpdesk.html">www.joomlalms.com/helpdesk.html</a><br />
-Vi garanterer et svar innen 2 arbeidsdager for spÃ¸rsmÃ¥l med normal prioritet og en dag for hastesaker<br /><br />
-Du kan ogsÃ¥ poste spÃ¸rsmÃ¥l pÃ¥ vÃ¥rt forum her <a href="http://www.joomlalms.com/forum/index.php">www.joomlalms.com/forum/index.php</a><br />
+$JLMS_LANGUAGE['_JLMS_SUPP_IF_HAVE_QUESTS'] = 'Dersom du har spørsmål/forslag eller opplever problemer med JoomlaLMS, vennligst kontakt vårt supportteam - <a href="http://www.joomlalms.com/helpdesk.html">www.joomlalms.com/helpdesk.html</a><br />
+Vi garanterer et svar innen 2 arbeidsdager for spørsmål med normal prioritet og en dag for hastesaker<br /><br />
+Du kan også poste spørsmål på vårt forum her <a href="http://www.joomlalms.com/forum/index.php">www.joomlalms.com/forum/index.php</a><br />
 Full dokumentasjon for JoomlaLMS finnes her <a href="http://www.joomlalms.com/lms-help/">www.joomlalms.com/lms-help/</a><br />
-Ofte stilte spÃ¸rsmÃ¥l: <a href="http://www.joomlalms.com/lms-help/faq/">www.joomlalms.com/lms-help/faq/</a>';
+Ofte stilte spørsmål: <a href="http://www.joomlalms.com/lms-help/faq/">www.joomlalms.com/lms-help/faq/</a>';
 ?>
