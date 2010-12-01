6409c9f50f12dfd214016ff3adf0ba7d131c27ce
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2010-12-01 08:02:57 +0000

    Legger over B15

--- kjerne/individuelle/Roar/j16/language/nb-NO/nb-NO.localise.php
+++ kjerne/individuelle/Roar/j16/language/nb-NO/nb-NO.localise.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		$Id: nb-NO.localise.php 1428 2010-11-28 19:22:06Z rued $
+ * @version		$Id$
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
