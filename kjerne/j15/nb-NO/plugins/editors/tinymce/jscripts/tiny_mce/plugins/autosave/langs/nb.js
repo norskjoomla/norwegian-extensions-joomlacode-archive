f2ed4dfe0bd2b095a1c182b8fad937f52c102370
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2011-01-24 14:55:38 +0000

    Buggfiks

--- kjerne/j15/nb-NO/plugins/editors/tinymce/jscripts/tiny_mce/plugins/autosave/langs/nb.js
+++ kjerne/j15/nb-NO/plugins/editors/tinymce/jscripts/tiny_mce/plugins/autosave/langs/nb.js
@@ -1,9 +1,4 @@
-// version 1.5.x $Id: nb.js 117 2006-10-24 12:49:07Z jens $
-// author Joomla! i Norge http://www.joomlainorge.no oversettelser@joomlainorge.no
-// copyright Copyright (C) 2005 - 2008 Joomla! i Norge. All rights reserved.
-// license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-
-tinyMCE.addToLang('',{
-autosave_unload_msg : 'Forandringene du gjorde forsvinner om du velger å forlate denne siden.'
+tinyMCE.addI18n('nb.autosave',{
+restore_content: "Gjenopprett autolagret innhold",
+warning_message: "Om du gjenoppretter lagret innhold vil du miste gjeldende endringer.\n\nEr du sikker på at du vil gjenopprette lagret innhold?"
 });
-
