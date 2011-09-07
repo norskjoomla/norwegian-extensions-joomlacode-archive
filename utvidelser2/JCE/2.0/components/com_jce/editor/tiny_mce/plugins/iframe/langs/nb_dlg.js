b6e2c06c8607aede1550fe6a3f1b8a4f27827300
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2011-09-07 22:59:24 +0000

    Meste oversatt, mangler litt i imagemanager_ext

--- utvidelser2/JCE/2.0/components/com_jce/editor/tiny_mce/plugins/iframe/langs/nb_dlg.js
+++ utvidelser2/JCE/2.0/components/com_jce/editor/tiny_mce/plugins/iframe/langs/nb_dlg.js
@@ -1,5 +1,5 @@
 tinyMCE.addI18n('nb.iframe_dlg',{
-is_external: "Nettadressen du fylte inn ser ut til å være en ekstern lenke,vil du legge tilpåkrevd http:// prefiks?",
+is_external: "Nettadressen du fylte inn ser ut til å være en ekstern lenke, vil du legge til påkrevd http://-prefiks?",
 no_src: 'Du må angi en nettadresse for den integrerte rammen',
 no_dimensions : 'Du må angi en bredde og høyde for den integrerte rammen'
 });
