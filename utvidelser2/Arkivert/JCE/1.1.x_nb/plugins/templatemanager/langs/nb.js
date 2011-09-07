854f94f7350207aeb0ebf8befca66e850daa28e1
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2011-09-07 23:05:01 +0000

    Arkiveres

---
+++ utvidelser2/Arkivert/JCE/1.1.x_nb/plugins/templatemanager/langs/nb.js
@@ -0,0 +1,14 @@
+// no-nb lang variables
+tinyMCE.addToLang('templatemanager',{
+desc : 'Sett inn/Lagre mal',
+save : 'Lagre mal',
+saving : 'Lagrer...',
+warning : 'Advarsel: Ved å oppdatere en mal med en annen, kan en miste data.',
+def_date_format : '%Y-%m-%d %H:%M:%S',
+months_long : new Array("Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"),
+months_short : new Array("Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"),
+day_long : new Array("Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"),
+day_short : new Array("Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"),
+delta_width : 0,
+delta_height : 0
+});
---
+++ utvidelser2/Arkivert/JCE/1.1.x_nb/plugins/visualchars/langs/nb.js
@@ -0,0 +1,7 @@
+// NB lang variables
+
+// Ferdig oversatt, 25. mai 2007
+
+tinyMCE.addToLang('visualchars',{
+desc : 'Visuelle formateringssymboler av/på.'
+});
