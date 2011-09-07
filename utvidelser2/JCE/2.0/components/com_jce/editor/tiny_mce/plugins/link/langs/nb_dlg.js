60e39ed978181db601165ab2af382e6110599e31
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2011-09-07 22:59:24 +0000

    Meste oversatt, mangler litt i imagemanager_ext

--- utvidelser2/JCE/2.0/components/com_jce/editor/tiny_mce/plugins/link/langs/nb_dlg.js
+++ utvidelser2/JCE/2.0/components/com_jce/editor/tiny_mce/plugins/link/langs/nb_dlg.js
@@ -1,7 +1,7 @@
 tinyMCE.addI18n('nb.link_dlg',{
 title:"Sett inn/Endre lenke",
-is_email:"Nettadressen du fylte inn ser ut til å være en e-postadresse, vil du legge til påkrevd mailto: prefiks?",
-is_external:"Nettadressen du fylte inn se rut til å være en ekstern lenke, vil du legge til påkrevd http:// prefiks?",
+is_email:"Nettadressen du fylte inn ser ut til å være en e-postadresse, vil du legge til påkrevd mailto:-prefiks?",
+is_external:"Nettadressen du fylte inn se rut til å være en ekstern lenke, vil du legge til påkrevd http://-prefiks?",
 no_href:"En nettadresse er påkrevd. Du må velge en lenke eller fylle inn en nettadresse",
 no_text:"Fyll inn en tekst for lenken"
 });
