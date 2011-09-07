8d7a4111f3085fb096e4c4fd569fab1b92d8fac7
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2011-09-07 23:04:26 +0000

    Arkiveres

--- utvidelser2/JCE/1.1.x_nb/plugins/caption/langs/nb.js
+++
@@ -1,8 +0,0 @@
-// NO-NB lang variables - Transl.:Ronny Buelund - Corr.:
-
-tinyMCE.addToLang('caption',{
-desc : 'Sett inn/Rediger bildetekst',
-delete_desc : 'Slett bildetekst',
-web_safe : 'Nettsikker',
-def : 'Standard'
-});
