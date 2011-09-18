627d165df2a66c114ca3bef77f4f35db9400923f
tommyrsk <tommyrsk@6aa871c5-9e2b-0410-bde1-d651c692654a>
2011-09-18 19:06:17 +0000

    Tilpasset, ryddet i filer og oversatt for JomSocial versjon 2.2.4.

--- utvidelser2/JomSocial/epostmaler/reports.email.php
+++
@@ -1,8 +0,0 @@
-Hei {target},
-
-Et rapportert element har nådd utløpsterskel og har blitt automatisk behandlet med standard handling.
-
-Du kan se det rapporterte elementet her:
-
-URL: <?php echo $url; ?>
-
