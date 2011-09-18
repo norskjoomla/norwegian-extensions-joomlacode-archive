fc0e4527bd3ed45b60e9b74259d76edc56ecebf8
Tommy Roger Skålvik <tommyrsk@users.noreply.github.com>
2011-09-18 19:06:17 +0000

    Tilpasset, ryddet i filer og oversatt for JomSocial versjon 2.2.4.

--- utvidelser2/JomSocial/emailtemplate/photos.email.tagging.php
+++
@@ -1,8 +0,0 @@
-Hi {target},
-
-{actor} tagged you in a photo.  To view this photo, please click the link below:
-
-<?php echo $url; ?>
-
-
-Have a nice day!
--- utvidelser2/JomSocial/emailtemplate/reports.email.php
+++
@@ -1,6 +0,0 @@
-Hi {target},
-
-A reported item has reached its threshold and the default action has been executed. The location of the reported item may be found below,
-
-URL: <?php echo $url; ?>
-
