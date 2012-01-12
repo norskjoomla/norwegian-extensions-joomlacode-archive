f89bac739186673dac88472c7903436f7c4aafc0
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2012-01-12 10:56:38 +0000

    Arkiverer 1.5 - Det ventes ingen flere endringer her

--- kjerne/j15/nn-NO/plugins/editors/tinymce/jscripts/tiny_mce/plugins/directionality/langs/nno-NO.js
+++
@@ -1,6 +0,0 @@
-// nn = Norwegian (nynorsk) lang variables by Knut B. Jacobsen
-
-tinyMCE.addToLang('',{
-directionality_ltr_desc : 'Venstre mot høgre',
-directionality_rtl_desc : 'Høgre mot venstre'
-});
