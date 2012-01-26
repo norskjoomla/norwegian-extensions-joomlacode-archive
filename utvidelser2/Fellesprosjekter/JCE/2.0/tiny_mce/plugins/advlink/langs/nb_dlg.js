3236c45c1e2cce1f87685859683f887d54ab54d0
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2012-01-26 18:02:54 +0000

    Litt i overkant ... ;)

--- utvidelser2/Fellesprosjekter/JCE/2.0/tiny_mce/plugins/advlink/langs/nb_dlg.js
+++
@@ -1,6 +0,0 @@
-tinyMCE.addI18n('nb.advlink_dlg',{
-title:"Sett inn/endre lenke",
-is_email:"Adressen du skrev inn ser ut som en epost-adresse. Vil du legge til den obligatoriske mailto: - prefikset?",
-is_external:"Adressen du skrev inn ser ut som en ekstern nettadresse. Vil du legge til den obligatoriske http:// - prefikset?",
-no_href:"En nettadresse er obligatorisk. Velg en lenke, eller skriv inn en adresse i adressefeltet."
-});
--- utvidelser2/Fellesprosjekter/JCE/2.0/tiny_mce/plugins/caption/langs/nb.js
+++
@@ -1,6 +0,0 @@
-// NO lang variables
-tinyMCE.addI18n('nb.caption',{
-desc : 'Sett inn/Endre bildetekst',
-'delete' : 'Slett bildetekst'
-});
-
