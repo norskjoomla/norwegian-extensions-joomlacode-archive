bb6405794f6ee8f0adb053da59d9aa082f7ed094
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2012-01-26 19:35:47 +0000

    Tilbake med ekstra

---
+++ utvidelser2/Fellesprosjekter/JCE/2.0/tiny_mce/plugins/filemanager/langs/nb_dlg.js
@@ -0,0 +1,10 @@
+// NO lang variables
+tinyMCE.addI18n('nb.filemanager_dlg',{
+icon: 'Ikon',
+size: 'Størrelse',
+text: 'Tekst',
+date: 'Dato',
+'replace_text': 'Bytt ut fillenketekst med filnavn?',
+no_src: 'Du må velge en fil eller en nettadresse',
+no_text: 'Tekst for fillenke er påkrevd'
+});
