66460b1aeb45d2a83112859afedcfeb1650aa6da
rued <rued@6aa871c5-9e2b-0410-bde1-d651c692654a>
2012-01-26 16:52:52 +0000

    Forenkler struktur

---
+++ utvidelser2/Fellesprosjekter/JCE/2.0/tiny_mce/plugins/advcode/langs/nb_dlg.js
@@ -0,0 +1,16 @@
+tinyMCE.addI18n('nb.advcode_dlg',{
+title:"Avansert code-editor",
+wordwrap:"Bryt lange linjer",
+highlight:"Marker",
+language:"Språk",
+numbers:"Linje nr",
+autocomplete:"Autofullfør",
+'fontsize':"Fontstørrelse",
+strong: 'Fet stil',
+em: 'Kursiv',
+underline: 'Understreket',
+removeformat: 'Fjern formatering',
+format: 'Format',
+undo: 'Angre',
+redo: 'Gjenta kommando'
+});
