712022f75a267b0cae7802ec68f6604144d16858
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2012-06-23 11:59:54 +0000

    Struktur

--- kjerne/arkivert/j15/nn-NO/plugins/editors/tinymce/jscripts/tiny_mce/plugins/iespell/langs/nno-NO.js
+++
@@ -1,6 +0,0 @@
-// nn = Norwegian (nynorsk) lang variables by Knut B. Jacobsen
-
-tinyMCE.addToLang('',{
-iespell_desc : 'Kjør rettstavingskontroll',
-iespell_download : "ieSpell virker ikke å være installert. Klikk OK for å laste hjem."
-});
